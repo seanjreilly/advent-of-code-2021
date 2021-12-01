1bebb942b8d5c077472ca9266f41681f9de49dd0
Sean Reilly <seanjreilly@gmail.com>
2021-12-01 13:36:37 +0000

    Move part functions outside of the main function
    (so they can be easily tested)

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,20 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var last = input[0].toInt()
-        var increases = 0
-        input.subList(1, input.size).map(String::toInt).forEach {
-            if (it > last) { increases++ }
-            last = it
-        }
-
-        return increases
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    var last = input[0].toInt()
+    var increases = 0
+    input.subList(1, input.size).map(String::toInt).forEach {
+        if (it > last) { increases++ }
+        last = it
+    }
+
+    return increases
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
 }
