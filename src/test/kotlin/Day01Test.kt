d1e1b17688764dbdb55937b94df9018bfb7bb11a
Sean Reilly <seanjreilly@gmail.com>
2021-12-01 14:03:59 +0000

    Add unit tests for sliding window functions

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -4,8 +4,32 @@ class Day01Test {
     @Test
     fun `list_windowed() creates three part sliding windows`() {
         val list = listOf(1,2,3,4,5,6,7,8,9,10)
-        val windowed = list.windowed(3, 1, false)
-        println(windowed)
+        val result = list.windowed(3, 1, false)
+        println(result)
+        assert(result.size == 8)
+        result.forEach { assert(it.size == 3) }
+    }
+
+    @Test
+    fun `list_windowed(2, 1) creates 9 windows with 2 elements each`() {
+        val list = listOf(1,2,3,4,5,6,7,8,9,10)
+        val result = list.windowed(2, 1, false)
+        println(result)
+        assert(result.size == 9)
+        result.forEach { assert(it.size == 2) }
+    }
+
+    @Test
+    fun `calculate the sum of a window`() {
+        val list = listOf(1,2,3)
+        val result = list.sum()
+        assert(result == 6)
+
+        val list2 = listOf(1,2,3,4,5,6,7,8,9,10)
+        val windows = list2.windowed(3, 1, false)
+        val windowSums = windows.map { it.sum() }
+        assert(windowSums.first() == 1 + 2 + 3)
+        assert(windowSums.last() == 8 + 9 + 10)
     }
 
     @Test
