10494faea2f9c874a01bf9f3d74035ff77e4a7de
Sean Reilly <seanjreilly@gmail.com>
2021-12-02 11:25:42 +0000

    Day 2, part 1
    Used a regex, which I'm not super happy about (but more robust
    if there's multiple whitespace, etc.), and a parameterised junit
    test for the instruction parsing

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -3,14 +3,45 @@ import readInput
 
 fun main() {
     val input = readInput("Day02")
-    println(part1(input))
-    println(part2(input))
+    val day02 = Day02()
+    println(day02.part1(input))
+    println(day02.part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    return input.size
-}
+class Day02 {
 
-fun part2(input: List<String>): Int {
-    return input.size
+    var depth = 0
+    var horizontalPosition = 0
+
+    enum class Direction {
+        up,
+        down,
+        forward
+    }
+
+    val directionActions: Map<Direction, (Int) -> Unit> = mapOf(
+        Direction.up to {value -> depth -= value},
+        Direction.down to {value -> depth += value},
+        Direction.forward to {value -> horizontalPosition += value }
+    )
+
+    fun part1(input: List<String>): Int {
+        input
+            .filter(String::isNotBlank)
+            .map(this::parse)
+            .forEach {(direction, distance) -> directionActions[direction]!!.invoke(distance) }
+        return depth * horizontalPosition
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    private val parseRegex = """\s*(\w+)\s+(\d+)\s*""".toRegex()
+
+    fun parse(input: String): Pair<Direction, Int> {
+        val match = requireNotNull(parseRegex.matchEntire(input)) { "invalid line '${input}'" }
+        val (rawDirection, value) = match.destructured
+        return Pair(Direction.valueOf(rawDirection), value.toInt())
+    }
 }
--- src/test/kotlin/day02/Day02Test.kt
+++ src/test/kotlin/day02/Day02Test.kt
@@ -1,4 +1,67 @@
 package day02.day02
 
+import day02.Day02
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
 class Day02Test {
+    private val systemUnderTest = Day02()
+
+    @Test
+    fun `part1 should ignore blank lines`() {
+        val input = listOf(
+            "",
+            "   ",
+            ""
+        )
+
+        val result = systemUnderTest.part1(input)
+
+        assert(result == 0)
+    }
+
+    @Test
+    fun `part1 should calculate the multiple of horizontal distance and depth`() {
+        val input = listOf(
+            "forward 5", //horizontal position 5, depth 0
+            "down 5", // horizontal position depth 5 (down *increases* depth)
+            "forward 8", //horizontal position 13, depth 5
+            "up 3", //horizontal position 13, depth 2 (up *decreases* depth)
+            "down 8", //horizontal position 13, depth 10
+            "forward 2", //horizontal position 15, depth 10
+        )
+
+        val result = systemUnderTest.part1(input)
+
+        assert(result == 150)
+    }
+
+    @ParameterizedTest
+    @MethodSource("parseInstructionValues")
+    fun `parseInstruction should return a direction and a value`(input : String, direction: Day02.Direction, distance: Int) {
+        val result : Pair<Day02.Direction, Int> = systemUnderTest.parse(input)
+        assert(result.first == direction)
+        assert(result.second == distance)
+    }
+
+    companion object {
+        @JvmStatic
+        fun parseInstructionValues() : List<Arguments> {
+            return listOf(
+                Arguments.of("forward 5", Day02.Direction.forward, 5),
+                Arguments.of("up 27", Day02.Direction.up, 27),
+                Arguments.of("down 9", Day02.Direction.down, 9)
+            )
+        }
+    }
+
+    @Test
+    fun `parseInstruction should ignore extra whitespace`() {
+        val input = "     forward    5   "
+        val result : Pair<Day02.Direction, Int> = systemUnderTest.parse(input)
+        assert(result.first == Day02.Direction.forward)
+        assert(result.second == 5)
+    }
 }
