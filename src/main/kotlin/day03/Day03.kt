f636d58b9ff368b509023df069dc3318d25813cd
Sean Reilly <seanjreilly@gmail.com>
2021-12-03 12:12:39 +0000

    Solved Day 3, part 2
    Stateful implementations that use manual iteration and mutate a
    collection in place. Would be nice to clean that up.

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -1,6 +1,7 @@
 package day03
 
 import readInput
+import kotlin.math.roundToInt
 
 fun main() {
     val input = readInput("Day03")
@@ -18,12 +19,36 @@ fun part1(input: List<String>): UInt {
     return gamma * epsilon
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(input: List<String>): UInt {
+    //find oxygen generator rating
+    val potentialOxygenGeneratorRatings: MutableList<BitArray> = input.toMutableList()
+    var round = 0
+    while (potentialOxygenGeneratorRatings.size > 1) {
+        val numberOfSetBitsInEachPosition = countNumberOfSetBitsInEachPosition(potentialOxygenGeneratorRatings)
+        val rawBinaryResult = convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition, potentialOxygenGeneratorRatings.size)
+        val filterValue = rawBinaryResult[round]
+        potentialOxygenGeneratorRatings.removeAll { it[round] != filterValue }
+        round++
+    }
+    val oxygenGeneratorRating = potentialOxygenGeneratorRatings.first().toUInt()
+
+    //find CO2 scrubber rating
+    val potentialCO2ScrubberRatings: MutableList<BitArray> = input.toMutableList()
+    round = 0
+    while (potentialCO2ScrubberRatings.size > 1) {
+        val numberOfSetBitsInEachPosition = countNumberOfSetBitsInEachPosition(potentialCO2ScrubberRatings)
+        val rawBinaryResult = convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition, potentialCO2ScrubberRatings.size)
+        val filterValue = rawBinaryResult[round].not()
+        potentialCO2ScrubberRatings.removeAll { it[round] != filterValue }
+        round++
+    }
+    val co2ScrubberRating = potentialCO2ScrubberRatings.first().toUInt()
+
+    return oxygenGeneratorRating * co2ScrubberRating
 }
 
 typealias BitArray = String
-fun BitArray.toUInt() = this.toUInt(2)
+private fun BitArray.toUInt() = this.toUInt(2)
 private fun BitArray.invert(): BitArray {
     //kotlin platform invert functions use two's complement which wrecks everything, so just use string replacement
     return this
@@ -31,11 +56,20 @@ private fun BitArray.invert(): BitArray {
         .replace('1', '0')
         .replace('X', '1')
 }
+//"negate" a Char that is masquerading as a bit
+private fun Char.not(): Char {
+    return when (this) {
+        '0' -> '1'
+        '1' -> '0'
+        else -> throw IllegalArgumentException("expected a 1 or 0")
+    }
+}
 
 //the only possible values for a bit are 1 and 0, so we can compare 1s to the total amount instead of also counting 0s
 private fun convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition: IntArray, totalNumberOfRows: Int): BitArray {
+    val threshold: Int = (totalNumberOfRows.toDouble() / 2).roundToInt()
     return numberOfSetBitsInEachPosition
-        .map { if (it > (totalNumberOfRows / 2)) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
+        .map { if (it >= threshold) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
         .joinToString("")
 }
 
--- src/test/kotlin/day03/Day03Test.kt
+++ src/test/kotlin/day03/Day03Test.kt
@@ -33,4 +33,33 @@ class Day03Test {
         //returned value is the product of gamma and epsilon = 22 * 9 = 198
         assert(result == expectedGammaValue * expectedEpsilonValue)
     }
+    
+    @Test
+    fun `part2 should calculate oxygen generator rating and CO2 scrubber rating and return their product`() {
+        /*
+        To calculate oxygen generator rating:
+        Start with all numbers and consider only the first bit of each number.
+        There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a 1 in the first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
+        Consider the second bit of the 7 remaining numbers: there are more 0 bits (4) than 1 bits (3), so keep only the 4 numbers with a 0 in the second position: 10110, 10111, 10101, and 10000.
+        In the third position, three of the four numbers have a 1, so keep those three: 10110, 10111, and 10101.
+        In the fourth position, two of the three numbers have a 1, so keep those two: 10110 and 10111.
+        In the fifth position, there are an equal number of 0 bits and 1 bits (one each). So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
+        As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
+        */
+        val expectedOxygenGeneratorRating = "10111".toUInt(2)
+
+        /*
+        To calculate CO2 scrubber rating:
+        Start with all 12 numbers and consider only the first bit of each number.
+        There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a 0 in the first position: 00100, 01111, 00111, 00010, and 01010.
+        Consider the second bit of the 5 remaining numbers: there are fewer 1 bits (2) than 0 bits (3), so keep only the 2 numbers with a 1 in the second position: 01111 and 01010.
+        In the third position, there are an equal number of 0 bits and 1 bits (one each). So, to find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
+        As there is only one number left, stop; the CO2 scrubber rating is 01010, or 10 in decimal.
+        */
+        val expectedCO2ScrubberRating = "01010".toUInt(2)
+
+        val result = part2(testInput)
+
+        assert(result == expectedOxygenGeneratorRating * expectedCO2ScrubberRating)
+    }
 }
