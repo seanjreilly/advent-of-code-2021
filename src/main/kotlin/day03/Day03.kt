d02d5e38a6e2dcfaded48b5a84aa2d3cad2d8f2d
Sean Reilly <seanjreilly@gmail.com>
2021-12-03 10:11:22 +0000

    Code cleanup
    extract some shorter private methods

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -9,6 +9,26 @@ fun main() {
 }
 
 fun part1(input: List<String>): UInt {
+    val numberOfSetBitsInEachPosition = countNumberOfSetBitsInEachPosition(input)
+
+    val rawBinaryResult = StringBuilder()
+    //the only possible values for a bit are 1 and 0, so we can compare 1s to the total amount instead of also counting 0s
+    numberOfSetBitsInEachPosition
+        .map { if (it > (input.size / 2)) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
+        .forEach(rawBinaryResult::append)
+
+    val gamma = rawBinaryResult.toString().toUInt(2)
+    val epsilon = invertRawBinaryResult(rawBinaryResult).toUInt(2) //least common bit in each field is just the inverse of the binary result for gamma
+
+    return gamma * epsilon
+}
+
+private fun invertRawBinaryResult(rawBinaryResult: StringBuilder): String {
+    //kotlin platform invert functions use two's complement which wrecks everything, so just use string replacement
+    return rawBinaryResult.toString().replace("0", "X").replace("1", "0").replace("X", "1")
+}
+
+private fun countNumberOfSetBitsInEachPosition(input: List<String>): IntArray {
     val oneBits = IntArray(input[0].length)
     input.forEach { line ->
         for (i in line.toCharArray().indices) {
@@ -17,20 +37,7 @@ fun part1(input: List<String>): UInt {
             }
         }
     }
-    val rawBinaryResult = StringBuilder()
-    oneBits
-        .map { if (it > (input.size/2)) '1' else '0' }
-        .forEach (rawBinaryResult::append)
-
-    //Let's use UInt because we're dealing with unsigned binary numbers
-    val gamma = rawBinaryResult.toString().toUInt(2)
-
-    //kotlin platform invert functions use two's complement which wrecks everything
-    val invertedRawBinaryResult = rawBinaryResult.toString().replace("0", "X").replace("1", "0").replace("X", "1")
-
-    val epsilon = invertedRawBinaryResult.toUInt(2)
-
-    return gamma * epsilon
+    return oneBits
 }
 
 fun part2(input: List<String>): Int {
