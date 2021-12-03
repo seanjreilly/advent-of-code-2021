632fc9f190b27b54bac75569d0ea6d7c7cf32e69
Sean Reilly <seanjreilly@gmail.com>
2021-12-03 10:39:56 +0000

    Code cleanup
    General clarity

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -13,17 +13,23 @@ fun part1(input: List<String>): UInt {
     val rawBinaryResult = convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition, input.size)
 
     val gamma = rawBinaryResult.toUInt()
-    //least common bit in each field is just the inverse of the binary result for gamma
-    val epsilon = rawBinaryResult.invert().toUInt()
+    val epsilon = rawBinaryResult.invert().toUInt() //least common bit in each field is just the binary inverse of gamma
 
     return gamma * epsilon
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
 }
 
 typealias BitArray = String
 fun BitArray.toUInt() = this.toUInt(2)
 private fun BitArray.invert(): BitArray {
     //kotlin platform invert functions use two's complement which wrecks everything, so just use string replacement
-    return this.replace("0", "X").replace("1", "0").replace("X", "1")
+    return this
+        .replace('0', 'X')
+        .replace('1', '0')
+        .replace('X', '1')
 }
 
 //the only possible values for a bit are 1 and 0, so we can compare 1s to the total amount instead of also counting 0s
@@ -40,8 +46,4 @@ private fun countNumberOfSetBitsInEachPosition(input: List<BitArray>): IntArray 
         .map { it.index }
         .forEach {oneBits[it]++}
     return oneBits
-}
-
-fun part2(input: List<String>): Int {
-    return input.size
 }
