4d787ca271e922a8c7915f9dc7f445c2a8f0c775
Sean Reilly <seanjreilly@gmail.com>
2021-12-03 10:06:22 +0000

    Day 3, part1
    Initial pass uses an old for construct instead of a stream to
    count 1 values in the initial position, and the code should be
    cleaned up

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -8,8 +8,29 @@ fun main() {
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun part1(input: List<String>): UInt {
+    val oneBits = IntArray(input[0].length)
+    input.forEach { line ->
+        for (i in line.toCharArray().indices) {
+            if (line[i] == '1') {
+                oneBits[i]++
+            }
+        }
+    }
+    val rawBinaryResult = StringBuilder()
+    oneBits
+        .map { if (it > (input.size/2)) '1' else '0' }
+        .forEach (rawBinaryResult::append)
+
+    //Let's use UInt because we're dealing with unsigned binary numbers
+    val gamma = rawBinaryResult.toString().toUInt(2)
+
+    //kotlin platform invert functions use two's complement which wrecks everything
+    val invertedRawBinaryResult = rawBinaryResult.toString().replace("0", "X").replace("1", "0").replace("X", "1")
+
+    val epsilon = invertedRawBinaryResult.toUInt(2)
+
+    return gamma * epsilon
 }
 
 fun part2(input: List<String>): Int {
--- src/test/kotlin/day03/Day03Test.kt
+++ src/test/kotlin/day03/Day03Test.kt
@@ -1,4 +1,35 @@
 package day03
 
+import org.junit.jupiter.api.Test
+
 class Day03Test {
+    
+    @Test
+    fun `part1 should calculate gamma and epsilon values and return their product`() {
+        val input = """
+            00100
+            11110
+            10110
+            10111
+            10101
+            01111
+            00111
+            11100
+            10000
+            11001
+            00010
+            01010
+        """.trimIndent().lines()
+
+        //gamma value is calculated from the most common bits in each position: 10110 = 22
+        val expectedGammaValue = "10110".toUInt(2)
+        //epsilon value is the from the least common bits in each position (and therefore the binary inverse of gamma): 01001 = 9
+        val expectedEpsilonValue = "01001".toUInt(2)
+
+        val result = part1(input)
+
+
+        //returned value is the product of gamma and epsilon = 22 * 9 = 198
+        assert(result == expectedGammaValue * expectedEpsilonValue)
+    }
 }
