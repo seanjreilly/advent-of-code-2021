cc991c2ca1d9b2f2623a4bda7d4145fa2dac3e1b
Sean Reilly <seanjreilly@gmail.com>
2021-12-03 10:28:36 +0000

    Code cleanup
    Extract a convertBitCountsToRawBinaryResult method

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -10,22 +10,28 @@ fun main() {
 
 fun part1(input: List<String>): UInt {
     val numberOfSetBitsInEachPosition = countNumberOfSetBitsInEachPosition(input)
+    val rawBinaryResult = convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition, input.size)
 
-    val rawBinaryResult = StringBuilder()
-    //the only possible values for a bit are 1 and 0, so we can compare 1s to the total amount instead of also counting 0s
-    numberOfSetBitsInEachPosition
-        .map { if (it > (input.size / 2)) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
-        .forEach(rawBinaryResult::append)
-
-    val gamma = rawBinaryResult.toString().toUInt(2)
-    val epsilon = invertRawBinaryResult(rawBinaryResult).toUInt(2) //least common bit in each field is just the inverse of the binary result for gamma
+    val gamma = rawBinaryResult.toUInt(2)
+    //least common bit in each field is just the inverse of the binary result for gamma
+    val epsilon = invertRawBinaryResult(rawBinaryResult).toUInt(2)
 
     return gamma * epsilon
 }
 
-private fun invertRawBinaryResult(rawBinaryResult: StringBuilder): String {
+//the only possible values for a bit are 1 and 0, so we can compare 1s to the total amount instead of also counting 0s
+private fun convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition: IntArray, numberOfRows: Int): String {
+    val rawBinaryResult = StringBuilder()
+    numberOfSetBitsInEachPosition
+        .map { if (it > (numberOfRows / 2)) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
+        .forEach(rawBinaryResult::append)
+    val rawBinaryResultString = rawBinaryResult.toString()
+    return rawBinaryResultString
+}
+
+private fun invertRawBinaryResult(rawBinaryResult: String): String {
     //kotlin platform invert functions use two's complement which wrecks everything, so just use string replacement
-    return rawBinaryResult.toString().replace("0", "X").replace("1", "0").replace("X", "1")
+    return rawBinaryResult.replace("0", "X").replace("1", "0").replace("X", "1")
 }
 
 private fun countNumberOfSetBitsInEachPosition(input: List<String>): IntArray {
