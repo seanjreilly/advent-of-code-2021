3a1d7a1c77c15111976266df02e21735bc286e4f
Sean Reilly <seanjreilly@gmail.com>
2021-12-03 10:34:20 +0000

    Code cleanup
    - Introduce a type alias "BitArray" for when we're using Strings
    as if they were bit arrays
    - Add extension functions for converting BitArrays to UInts and
    doing binary inversion

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -12,29 +12,31 @@ fun part1(input: List<String>): UInt {
     val numberOfSetBitsInEachPosition = countNumberOfSetBitsInEachPosition(input)
     val rawBinaryResult = convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition, input.size)
 
-    val gamma = rawBinaryResult.toUInt(2)
+    val gamma = rawBinaryResult.toUInt()
     //least common bit in each field is just the inverse of the binary result for gamma
-    val epsilon = invertRawBinaryResult(rawBinaryResult).toUInt(2)
+    val epsilon = rawBinaryResult.invert().toUInt()
 
     return gamma * epsilon
 }
 
+typealias BitArray = String
+fun BitArray.toUInt() = this.toUInt(2)
+private fun BitArray.invert(): BitArray {
+    //kotlin platform invert functions use two's complement which wrecks everything, so just use string replacement
+    return this.replace("0", "X").replace("1", "0").replace("X", "1")
+}
+
 //the only possible values for a bit are 1 and 0, so we can compare 1s to the total amount instead of also counting 0s
-private fun convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition: IntArray, numberOfRows: Int): String {
+private fun convertBitCountsToRawBinaryResult(numberOfSetBitsInEachPosition: IntArray, totalNumberOfRows: Int): BitArray {
     val rawBinaryResult = StringBuilder()
     numberOfSetBitsInEachPosition
-        .map { if (it > (numberOfRows / 2)) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
+        .map { if (it > (totalNumberOfRows / 2)) '1' else '0' } //unspecified condition in the question: we can have a tie if the numbers are equal with an even number of input rows!!!
         .forEach(rawBinaryResult::append)
     val rawBinaryResultString = rawBinaryResult.toString()
     return rawBinaryResultString
 }
 
-private fun invertRawBinaryResult(rawBinaryResult: String): String {
-    //kotlin platform invert functions use two's complement which wrecks everything, so just use string replacement
-    return rawBinaryResult.replace("0", "X").replace("1", "0").replace("X", "1")
-}
-
-private fun countNumberOfSetBitsInEachPosition(input: List<String>): IntArray {
+private fun countNumberOfSetBitsInEachPosition(input: List<BitArray>): IntArray {
     val oneBits = IntArray(input[0].length)
     input.flatMap { it.toCharArray().withIndex() }
         .filter { it.value == '1' }
