fa9842121445b06457bf8ec9b7504ac9c6c1bc18
Sean Reilly <seanjreilly@gmail.com>
2021-12-04 19:23:42 +0000

    Add blank template for day 4

---
+++ src/main/kotlin/day04/Day04.kt
@@ -0,0 +1,17 @@
+package day04
+
+import readInput
+
+fun main() {
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
