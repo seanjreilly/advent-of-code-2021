358e621262c8b1b9a47bd7099d9be6ca3730c3e6
Sean Reilly <seanjreilly@gmail.com>
2021-12-06 12:05:49 +0000

    Day 6, part 1

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -3,15 +3,43 @@ package day06
 import readInput
 
 fun main() {
-    val input = readInput("Day06")
+    val input = readInput("Day06").single()
     println(part1(input))
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun part1(input: String): Int {
+    var count = parseFishCount(input)
+    (1..80).forEach { _ -> count = count.nextDay() }
+    return count.totalFish()
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(input: String): Int {
+    return input.length
+}
+
+internal fun parseFishCount(input: String): FishCount {
+    val initialCounts: Map<Int, Int> = input
+        .split(',')
+        .map(String::toInt)
+        //count the number of fish with each timer state
+        .groupingBy { it }.eachCount()
+        .withDefault { 0 }
+
+    return FishCount((0..8).map { initialCounts[it] ?: 0 }.toList()) //there are zero fish with any timer state not mentioned in the input
+}
+
+class FishCount(internal val counts: List<Int>) {
+    internal constructor(zero: Int, one: Int, two: Int, three: Int, four: Int, five: Int, six: Int, seven: Int, eight: Int) : this(
+        listOf(zero, one, two, three, four, five, six, seven, eight)
+    )
+
+    fun nextDay(): FishCount {
+        val nextCounts = counts.drop(1).toMutableList() // creates a copy shifted left one
+        nextCounts.add(counts[0]) //new fish created with timer 8
+        nextCounts[6] += counts[0] //existing fish move to timer 6
+        return FishCount(nextCounts)
+    }
+
+    fun totalFish(): Int = counts.sum()
 }
--- src/test/kotlin/day06/Day06Test.kt
+++ src/test/kotlin/day06/Day06Test.kt
@@ -1,4 +1,74 @@
 package day06
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
 class Day06Test {
+
+    @Test
+    fun `part1 should count the total number of fish after 80 days`() {
+        val result = part1("3,4,3,1,2")
+
+        assert(result == 5934)
+    }
+
+    @Test
+    fun `totalFishshould return the total number of fish`() {
+        val fishCount = FishCount(0, 1, 2, 3, 4, 5, 6, 7, 8)
+
+        val result = fishCount.totalFish()
+
+        assert(result == 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+    }
+
+    @Test
+    fun `parseFishCount should return a fish count with the appropriate number of fish in each frame`() {
+        val input = "3,4,3,1,2"
+
+        val result:FishCount = parseFishCount(input)
+
+        assert(result.counts == listOf(0,1,1,2,1,0,0,0,0))
+    }
+
+    @Nested
+    inner class FishCountTest {
+        @Test
+        fun `nextDay should produce a new FishCount with values from 8 through 1 decremented by 1`() {
+            val fishCount = FishCount(0, 1, 2, 3, 4, 5, 6, 7, 8)
+
+            val result: FishCount = fishCount.nextDay()
+
+            assert(result.counts.subList(0, 8) == listOf(1, 2, 3, 4, 5, 6, 7, 8))
+        }
+
+        @Test
+        fun `nextDay should produce a new FishCount with values from 0 added to 8 and to 6`() {
+            val fishCount = FishCount(2, 0, 0, 0, 0, 0, 0, 1, 0)
+
+            val result: FishCount = fishCount.nextDay()
+
+            // the "zero" fish move to 6, and a new fish is added to 8 for each zero fish
+            assert(result.counts == listOf(0, 0, 0, 0, 0, 0, 3, 0, 2))
+        }
+
+        @Test
+        fun `a fish count with known input should produce the correct value after 18 days`() {
+            var count = parseFishCount("3,4,3,1,2")
+            (1..18).forEach { count = count.nextDay() }
+
+            val result = count.totalFish()
+
+            assert(result == 26)
+        }
+
+        @Test
+        fun `a fish count with known input should produce the correct value after 80 days`() {
+            var count = parseFishCount("3,4,3,1,2")
+            (1..80).forEach { count = count.nextDay() }
+
+            val result = count.totalFish()
+
+            assert(result == 5934)
+        }
+    }
 }
