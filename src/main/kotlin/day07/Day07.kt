a7157b9137136504f2fc63b62bac05530c7902bb
Sean Reilly <seanjreilly@gmail.com>
2021-12-07 10:07:08 +0000

    Day 7, Part 1
    No modelling today, just ramming it in. Will clean later.

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,17 +1,29 @@
 package day07
 
 import readInput
+import kotlin.math.abs
 
 fun main() {
-    val input = readInput("Day07")
+    val input = readInput("Day07").first()
     println(part1(input))
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun part1(input: String): Int {
+    val startingHorizontalPositions = input.split(",").map(String::toInt)
+    //bangs are ok because the list is not empty
+    val minHorizontalPosition = startingHorizontalPositions.minOrNull()!!
+    val maxHorizontalPosition = startingHorizontalPositions.maxOrNull()!!
+    val deDupedStartingHorizontalPositions = startingHorizontalPositions.groupingBy { it }.eachCount() //key is position, value is count
+
+    val result = (minHorizontalPosition..maxHorizontalPosition).map { proposedFinalPosition ->
+        deDupedStartingHorizontalPositions.map { (startingPosition, numberOfCrabs) ->
+            abs(proposedFinalPosition - startingPosition) * numberOfCrabs
+        }.sum()
+    }.minOrNull()!!
+    return result
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(input: String): Int {
+    return input.length
 }
--- src/test/kotlin/day07/Day07Test.kt
+++ src/test/kotlin/day07/Day07Test.kt
@@ -1,4 +1,14 @@
 package day07
 
+import org.junit.jupiter.api.Test
+
 class Day07Test {
+    @Test
+    fun `part1 should find the horizontal position all crabs can move to using the least fuel given a list of starting positions and return the fuel cost`() {
+        val input = "16,1,2,0,4,2,7,1,2,14"
+
+        val result = part1(input)
+
+        assert(result == 37)
+    }
 }
