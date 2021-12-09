b316c659352a508bc282dbcfbd3dcd8565c328dc
Sean Reilly <seanjreilly@gmail.com>
2021-12-09 09:11:45 +0000

    Add blank template for day 9

---
+++ src/main/kotlin/day09/Day09.kt
@@ -0,0 +1,17 @@
+package day09
+
+import readInput
+
+fun main() {
+    val input = readInput("Day09")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
---
+++ src/test/kotlin/day09/Day09Test.kt
@@ -0,0 +1,4 @@
+package day09
+
+class Day09Test {
+}
