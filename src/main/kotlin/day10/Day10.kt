62016b77da4bacb4ef7936f1f886dc5541c70ae4
Sean Reilly <seanjreilly@gmail.com>
2021-12-10 10:41:58 +0000

    Day 10, part 1
    Using a stack to parse chunks instead of a regex pays off nicely
    in terms of complexity.

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -9,9 +9,77 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val scores = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+    )
+
+    return input
+        .asSequence()
+        .map { parseChunks(it) }
+        .filterIsInstance<ParseResult.Corrupted>()
+        .map { it.actual }
+        .map {scores[it]!! }
+        .sum()
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+internal sealed class ParseResult {
+
+    /**
+     * Represents a complete (uncorrupted) line
+     *
+     * A complete line is where every chunk is closed with the correct
+     * closing character that corresponds to the character that opened it
+     */
+    object Complete : ParseResult()
+
+    /**
+     * Represents an incomplete line
+     *
+     * An incomplete line is a line that has open chunks that aren't closed,
+     * but hasn't encountered an unexpected closing character
+     */
+    object Incomplete : ParseResult()
+
+    /**
+     * Represents a corrupted line
+     *
+     * A corrupted line is a line that closes a chunk with a character that
+     * isn't the one that opened the currently open chunk.
+     */
+    data class Corrupted(val expected: Char, val actual: Char) : ParseResult()
+}
+
+private val chunkDelimiters = mapOf(
+    '(' to ')',
+    '{' to '}',
+    '[' to ']',
+    '<' to '>'
+)
+
+internal fun parseChunks(line: String): ParseResult {
+    val stack = mutableListOf<Char>()
+
+    line.toCharArray().forEach { char ->
+        if (char in chunkDelimiters.keys) {
+            stack.add(char)
+        } else {
+            val expectedClosingCharacter = chunkDelimiters[stack.removeLast()]!!
+            if (char != expectedClosingCharacter) {
+                return ParseResult.Corrupted(expectedClosingCharacter, char)
+            }
+        }
+    }
+
+    if (stack.isEmpty()) {
+        return ParseResult.Complete
+    }
+
+    return ParseResult.Incomplete
 }
--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -1,4 +1,83 @@
 package day10
 
+import org.junit.jupiter.api.Test
+
 class Day10Test {
+    val input = """
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
+    """.trimIndent().lines()
+    
+    @Test
+    fun `part1 should parse chunks, find the score for the first illegal character on each line, and return the sum of the scores`() {
+        assert(part1(input) == 26397)
+    }
+    
+    @Test
+    fun `parseChunks should return Legal given a complete line`() {
+        val completeLines = listOf(
+            "()",
+            "[]",
+            "{}", //extra addition not included in the problem description
+            "<>", //extra addition not included in the problem description
+            "([])",
+            "{()()()}",
+            "<([{}])>",
+            "[<>({}){}[([])<>]]",
+            "(((((((((())))))))))"
+        )
+
+        completeLines.forEach {
+            assert(parseChunks(it) == ParseResult.Complete)
+        }
+    }
+
+    @Test
+    fun `parseChunks should return Incomplete given an incomplete line`() {
+        val incompleteLines = listOf(
+            "(",
+            "[",
+            "{",
+            "<",
+            "((",
+            "({",
+            "({[<",
+            "()("
+        )
+
+        incompleteLines.forEach {
+            assert(parseChunks(it) == ParseResult.Incomplete)
+        }
+    }
+
+    @Test
+    fun `parseChunks should return Corrupted (and report the expected and actual character) given a corrupted line`() {
+        data class Input(val line: String, val expected: Char, val actual: Char)
+
+        val corruptedLines = listOf(
+            Input("{([(<{}[<>[]}>{[]{[(<()>", ']', '}'),
+            Input("[[<[([]))<([[{}[[()]]]", ']', ')'),
+            Input("[{[{({}]{}}([{[{{{}}([]", ')', ']'),
+            Input("[<(<(<(<{}))><([]([]()", '>', ')'),
+            Input("<{([([[(<>()){}]>(<<{{", ']', '>')
+        )
+
+        corruptedLines.forEach { input ->
+            val result = parseChunks(input.line)
+
+            assert(result is ParseResult.Corrupted)
+            if (result is ParseResult.Corrupted) { //need this for a smart cast even though there's already an assert statement. Boo.
+                assert(result.expected == input.expected)
+                assert(result.actual == input.actual)
+            }
+        }
+    }
 }
