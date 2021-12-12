4b68fdca6358d5ac559b7f0a4f1520e804620a6a
Sean Reilly <seanjreilly@gmail.com>
2021-12-12 20:38:01 +0000

    Day 12, part 1
    Use a breadth-first search to find all possible paths, tracking
    validity (not entering any individual small cave more than once)
    before new candidate paths are added to the bfs queue.

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -9,9 +9,60 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    return findDistinctPaths(parseGraph(input)).size
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+internal fun parseGraph(input: List<String>): Graph {
+    return input
+        .map { it.split('-') }
+        .flatMap { listOf(listOf(it[0], it[1]), listOf(it[1], it[0])) } //if a links to b, b links to a
+        .groupBy({ it[0] }, { it[1] })
+        .mapValues { it.value.toSet()  }
+}
+
+internal typealias Graph = Map<String, Set<String>> //key is source vertex, value is set of vertices connected to source
+
+internal fun isSmallCave(cave: String): Boolean {
+    return cave.first().isLowerCase()
+}
+
+internal typealias Path = List<String>
+
+/*
+    Valid paths never visit any small cave more than once
+ */
+internal fun Path.isValid() : Boolean {
+    return this.filter { isSmallCave(it) }
+        .groupingBy { it }
+        .eachCount()
+        .filter { it.value > 1 }
+        .isEmpty()
+}
+
+internal fun findDistinctPaths(graph: Graph): Set<Path> {
+    val completedPaths = mutableSetOf<Path>()
+    val queue = mutableListOf(listOf("start"))
+    do {
+        val currentPath = queue.removeAt(0)
+        val nextVerticesToConsider = graph[currentPath.last()]!!
+        val nextPathsToConsider = nextVerticesToConsider
+            .map { currentPath + it }
+            .filter { it.isValid() }
+
+        //add any complete paths to the result
+        nextPathsToConsider
+            .filter { it.last() == "end" }
+            .forEach(completedPaths::add)
+
+        //add any incomplete paths to the queue
+        nextPathsToConsider
+            .filter { it.last() != "end" }
+            .forEach(queue::add)
+    } while (queue.isNotEmpty())
+
+    return completedPaths
 }
--- src/test/kotlin/day12/Day12Test.kt
+++ src/test/kotlin/day12/Day12Test.kt
@@ -1,4 +1,114 @@
 package day12
 
+import org.junit.jupiter.api.Test
+
 class Day12Test {
+    private val smallInput = """
+        start-A
+        start-b
+        A-c
+        A-b
+        b-d
+        A-end
+        b-end
+    """.trimIndent().lines()
+
+    @Test
+    fun `parseGraph should return an undirected graph given a list of edges`() {
+        val graph : Graph = parseGraph(smallInput)
+
+        //graph should have 6 vertices
+        assert(graph.keys.size == 6)
+        assert(graph.values.flatten().distinct().size == 6)
+
+        //check edges
+        assert(graph["start"] == setOf("A", "b"))
+        assert(graph["A"] == setOf("b", "c", "start", "end"))
+        assert(graph["b"] == setOf("A", "d", "start", "end"))
+        assert(graph["c"] == setOf("A"))
+        assert(graph["d"] == setOf("b"))
+        assert(graph["end"] == setOf("A", "b"))
+    }
+
+    @Test
+    fun `isSmallCave should return true if the cave name is lower case and false otherwise`() {
+        assert(!isSmallCave("A"))
+        assert(isSmallCave("b"))
+        assert(isSmallCave("c"))
+        assert(isSmallCave("d"))
+        assert(isSmallCave("start"))
+        assert(isSmallCave("end"))
+    }
+
+    @Test
+    fun `findDistinctPaths should return the distinct paths that start at 'start', end at 'end', and visit each small cave at most once`() {
+        val graph : Graph = parseGraph(smallInput)
+
+        val paths: Set<Path> = findDistinctPaths(graph)
+
+        val expectedResults = """
+            start,A,b,A,c,A,end
+            start,A,b,A,end
+            start,A,b,end
+            start,A,c,A,b,A,end
+            start,A,c,A,b,end
+            start,A,c,A,end
+            start,A,end
+            start,b,A,c,A,end
+            start,b,A,end
+            start,b,end
+        """.trimIndent().lines().map { line -> line.trim().split(',') }.toSet()
+
+        assert(expectedResults == paths)
+    }
+
+    @Test
+    fun `part1 should return the number of distinct paths through the cave system`() {
+        assert(part1(smallInput) == 10)
+    }
+
+    @Test
+    fun `part1 should work with a medium example`() {
+        val input = """
+            dc-end
+            HN-start
+            start-kj
+            dc-start
+            dc-HN
+            LN-dc
+            HN-end
+            kj-sa
+            kj-HN
+            kj-dc
+        """.trimIndent().lines()
+
+        assert(part1(input) == 19)
+    }
+
+    @Test
+    fun `part1 should work with a large example`() {
+        val input = """
+            fs-end
+            he-DX
+            fs-he
+            start-DX
+            pj-DX
+            end-zg
+            zg-sl
+            zg-pj
+            pj-he
+            RW-he
+            fs-DX
+            pj-RW
+            zg-RW
+            start-pj
+            he-WI
+            zg-he
+            pj-fs
+            start-RW
+        """.trimIndent().lines()
+
+        assert(part1(input) == 226)
+    }
 }
+
