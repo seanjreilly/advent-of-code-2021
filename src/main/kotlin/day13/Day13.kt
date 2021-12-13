eea1e7940aae8b604bc98ed3a713a8cfbc3e365c
Sean Reilly <seanjreilly@gmail.com>
2021-12-13 09:05:14 +0000

    Add blank template for day 13

---
+++ src/main/kotlin/day13/Day13.kt
@@ -0,0 +1,17 @@
+package day13
+
+import utils.readInput
+
+fun main() {
+    val input = readInput("Day13")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
---
+++ src/test/kotlin/day13/Day13Test.kt
@@ -0,0 +1,4 @@
+package day13
+
+class Day13Test {
+}
