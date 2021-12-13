b07d7adfb596acb7b8889e66c8f4282be62a84f8
Sean Reilly <seanjreilly@gmail.com>
2021-12-13 11:17:40 +0000

    Day 13, part 1
    Implement a sparse map and a fold operation
    The sparse map is fairly verbose, but it makes the fold operation
    easy to specify.

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -1,5 +1,6 @@
 package day13
 
+import utils.gridmap.Point
 import utils.readInput
 
 fun main() {
@@ -9,9 +10,115 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val map = SparseBooleanMap.parse(input)
+    val foldInstruction = FoldInstruction.parse(input).first()
+    val newMap = map.performFold(foldInstruction)
+    return newMap.count { newMap [it]}
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+internal class SparseBooleanMap(private val setPoints: Set<Point>) : Iterable<Point> {
+    val width: Int = setPoints.maxOf { it.x } + 1
+    val height: Int = setPoints.maxOf { it.y } + 1
+
+    operator fun get(point: Point) = setPoints.contains(point)
+
+    override fun iterator() = iterator {
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                yield(Point(x, y))
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (setPoints != (other as SparseBooleanMap).setPoints) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return setPoints.hashCode()
+    }
+
+    fun performFold(fold: FoldInstruction): SparseBooleanMap {
+        val transform  = { point:Point ->
+            if (fold.foldAlong == FoldAxis.y) {
+                if (point.y < fold.foldValue) {
+                    point
+                } else {
+                    Point(point.x, fold.foldValue - (point.y - fold.foldValue))
+                }
+            } else {
+                if (point.x < fold.foldValue) {
+                    point
+                } else {
+                    Point(fold.foldValue - (point.x - fold.foldValue), point.y)
+                }
+            }
+        }
+
+        val newSetPoints = setPoints
+            .map(transform)
+            .toSet()
+
+        return SparseBooleanMap(newSetPoints)
+    }
+
+    companion object {
+        fun parse(inputPoints: List<String>): SparseBooleanMap {
+            val points = inputPoints
+                .takeUntil { it.isBlank() }
+                .map { it.split(',').map(String::toInt) }
+                .map { (x,y) -> Point(x, y) }
+                .toSet()
+
+            return SparseBooleanMap(points)
+        }
+    }
+}
+
+@Suppress("EnumEntryName") //enums need to match the case in the input & input file
+internal enum class FoldAxis {
+    x,
+    y
+}
+
+internal data class FoldInstruction(val foldAlong: FoldAxis, val foldValue: Int) {
+    companion object {
+        fun parse(input: List<String>) : List<FoldInstruction> {
+            return input
+                .dropUntil { it.isBlank() }
+                .drop(1)
+                .map { it.split('=') }
+                .map { Pair( it[0].takeLast(1), it[1].toInt())}
+                .map { Pair(FoldAxis.valueOf(it.first), it.second) }
+                .map { FoldInstruction(it.first, it.second) }
+        }
+    }
+}
+
+/**
+ * Drops elements from the collection until the first element
+ * that satisfies a predicate, and then returns all remaining elements, whether they satisfy the predicate
+ * or not
+ */
+internal fun <T> List<T>.dropUntil(predicate: (T) -> Boolean): List<T> {
+    val firstIndex = this.indexOfFirst(predicate)
+    return if (firstIndex > 0) { this.subList(firstIndex, this.size) } else { this }
+}
+
+/**
+ * Allows elements from the collection up to but not including the first element
+ * that satisfies a predicate. All remaining elements are suppressed whether they satisfy the predicate
+ * or not
+ */
+fun <T> List<T>.takeUntil(predicate: (T) -> Boolean): List<T> {
+    val firstIndex = this.indexOfFirst(predicate)
+    return if (firstIndex > 0) { this.subList(0, firstIndex) } else { this }
 }
--- src/test/kotlin/day13/Day13Test.kt
+++ src/test/kotlin/day13/Day13Test.kt
@@ -1,4 +1,165 @@
 package day13
 
+import org.junit.jupiter.api.Nested
+
+import org.junit.jupiter.api.Test
+import utils.gridmap.Point
+
 class Day13Test {
+    private val sampleInput = """
+        6,10
+        0,14
+        9,10
+        0,3
+        10,4
+        4,11
+        6,0
+        6,12
+        4,1
+        0,13
+        10,12
+        3,4
+        3,0
+        8,4
+        1,10
+        2,14
+        8,10
+        9,0
+
+        fold along y=7
+        fold along x=5
+    """.trimIndent().lines()
+
+    private val sampleInputPoints = sampleInput.dropLast(3)
+
+    private val sampleInputFoldInstructions = sampleInput.takeLast(3)
+
+    @Test
+    fun `part1 should perform the first fold and return the number of points set`() {
+        assert(part1(sampleInput) == 17)
+    }
+
+    @Nested
+    inner class SparseBooleanMapTest {
+
+        @Test
+        fun `parse should return a rectangular map with only the specified points set`() {
+            val map: SparseBooleanMap = SparseBooleanMap.parse(sampleInputPoints)
+
+            assert(map.width == 11)
+            assert(map.height == 15)
+        }
+
+        @Test
+        fun `parse should ignore any lines after a blank line`() {
+            val expectedMap = SparseBooleanMap.parse(sampleInputPoints)
+            val map = SparseBooleanMap.parse(sampleInput)
+
+            assert(map == expectedMap)
+        }
+
+        @Test
+        fun `SparseMap should report the points set in its input as set, and any others as unset`() {
+            val map: SparseBooleanMap = SparseBooleanMap.parse(sampleInputPoints)
+            val expectedTotalNumberOfPoints = 11 * 15 //width and height of the map based on biggest x and y values
+            val expectedNumberOfSetPoints = 18
+            val expectedNumberOfUnsetPoints = expectedTotalNumberOfPoints - expectedNumberOfSetPoints
+
+            val (setPoints, unsetPoints) = map.partition { map[it] }
+
+            assert(setPoints.size == expectedNumberOfSetPoints)
+            assert(unsetPoints.size == expectedNumberOfUnsetPoints)
+        }
+
+        @Test
+        fun `fold should return a new map with the bottom rows of the map folded up given a fold instruction with a y axis`() {
+            val map = SparseBooleanMap.parse(sampleInputPoints)
+            val foldInstruction = FoldInstruction(FoldAxis.y, 7)
+            val expectedSetPoints = """
+                #.##..#..#.
+                #...#......
+                ......#...#
+                #...#......
+                .#.#..#.###
+                ...........
+                ...........
+            """.trimIndent().lines().toPointList()
+            assert(expectedSetPoints.size == 17) {"precondition failed — expected points not calculated correctly"}
+
+            val newMap : SparseBooleanMap = map.performFold(foldInstruction)
+
+            assert(newMap.width == map.width) //vertical fold shouldn't change the width
+            assert(newMap.height <= foldInstruction.foldValue) //if the last lines are empty the map might be smaller
+            expectedSetPoints.forEach { assert(newMap[it]) }
+            assert(newMap.count { newMap[it] } == expectedSetPoints.size)
+        }
+
+        @Test
+        fun `fold should return a new map with the rightmost columns of the map folded left given a fold instruction with an x axis`() {
+
+            val originalMapPoints = """
+                #.##..#..#.
+                #...#......
+                ......#...#
+                #...#......
+                .#.#..#.###
+                ...........
+                ...........
+            """.trimIndent().lines().toPointList()
+            val map = SparseBooleanMap(originalMapPoints.toSet())
+
+            val expectedMapPoints = """
+                #####
+                #...#
+                #...#
+                #...#
+                #####
+                .....
+                .....
+            """.trimIndent().lines().toPointList()
+            assert(expectedMapPoints.size == 16) {"precondition failed — expected points not calculated correctly"}
+
+            val foldInstruction = FoldInstruction(FoldAxis.x, 5)
+
+            val newMap = map.performFold(foldInstruction)
+
+            assert(newMap.height == map.height) //horizontal fold shouldn't change the height
+            assert(newMap.width <= foldInstruction.foldValue) //if the last columns are empty the map might be smaller
+            expectedMapPoints.forEach { assert(newMap[it]) }
+            assert(newMap.count { newMap[it] } == expectedMapPoints.size)
+        }
+    }
+
+    @Nested
+    inner class FoldInstructionTest {
+
+        @Test
+        fun `parse should return a list of fold instructions`() {
+            val foldInstructions:List<FoldInstruction> = FoldInstruction.parse(sampleInputFoldInstructions)
+
+            assert(foldInstructions.size == 2)
+            assert(foldInstructions[0].foldAlong == FoldAxis.y)
+            assert(foldInstructions[0].foldValue == 7)
+            assert(foldInstructions[1].foldAlong == FoldAxis.x)
+            assert(foldInstructions[1].foldValue == 5)
+        }
+
+        @Test
+        fun `parse should ignore any lines before a blank line`() {
+            val instructionsWithoutLeadingLines = FoldInstruction.parse(sampleInputFoldInstructions)
+            val instructionsFromOverallInput = FoldInstruction.parse(sampleInput)
+
+            assert(instructionsWithoutLeadingLines == instructionsFromOverallInput)
+        }
+    }
+
+}
+
+private fun List<String>.toPointList(): List<Point> {
+    return this
+        .flatMapIndexed { y, line ->
+            line.mapIndexed { x, char -> Pair(x, char) }
+                .filter { (_, char) -> char == '#' }
+                .map { (x, _) -> Point(x, y) }
+        }
 }
