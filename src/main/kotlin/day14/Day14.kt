e728bfd3405176064ceb5c6bb364f87ead24ad31
Sean Reilly <seanjreilly@gmail.com>
2021-12-14 10:13:48 +0000

    Day 14, part 1
    Hand-build a search and replace, partly for efficiency, but
    mostly to satisfy the rules of not processing new pairs mid-step
    and to not need to mangle indices for future insertions every
    time we insert something.
    
    Use of type aliases and value classes so everything isn't
    String to String, etc.

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -9,9 +9,78 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val polymerTemplate = parsePolymerTemplate(input)
+    val rules = parsePairInsertionRules(input)
+    (1..10).forEach { _ -> polymerTemplate.step(rules) }
+    val counts = polymerTemplate.counts().values
+    return counts.maxOrNull()!! - counts.minOrNull()!!
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+internal typealias PairInsertionRules = Map<PolymerPair, PolymerInsertion>
+
+@JvmInline
+internal value class PolymerPair(internal val pair: String) {
+    init { require(pair.length == 2) { "insertion pairs must have length 2" } }
+}
+
+@JvmInline
+internal value class PolymerInsertion(internal val element: Char)
+
+internal data class PolymerTemplate(private val polymer: StringBuilder) {
+    internal constructor(polymerTemplate: String) : this(StringBuilder(polymerTemplate))
+    override fun toString(): String = polymer.toString()
+    fun step(rules: PairInsertionRules) {
+        val matches = findWhereRulesMatch(rules)
+
+        //find the additional elements to insert
+        val insertionsToPerform = matches
+            .map { Pair(it.first, rules[it.second]!!) } //we know the values are in the map because we matched based on the keys
+
+        //insert the elements starting from the end of the polymer
+        // (this means that we don't need to track updated indices for the matches as we insert elements)
+        insertionsToPerform.reversed().forEach { (index, insertion) ->
+            polymer.insert(index + 1, insertion.element) //insert in the middle of the matched pair
+        }
+    }
+
+    /*
+        We use this custom traversal instead of a stream because it iterates through the polymer
+        only once even though there are many strings to find. This will be important (O(n) or thereabouts per invocation)
+        when the polymer gets longer.
+     */
+    private fun findWhereRulesMatch(rules: PairInsertionRules): List<Pair<Int, PolymerPair>> {
+        val pairsToFind = rules.keys.map { it.pair }
+        val matches = mutableListOf<Pair<Int, String>>()
+
+        //find all matches in the string
+        var startIndex: Int
+        var pairFound = polymer.findAnyOf(pairsToFind, 0)
+
+        while (pairFound != null) {
+            matches.add(pairFound)
+            startIndex = pairFound.first + 1
+            pairFound = polymer.findAnyOf(pairsToFind, startIndex)
+        }
+        return matches.map { Pair(it.first, PolymerPair(it.second)) }
+    }
+
+    internal fun counts() : Map<Char, Int> {
+        return polymer.groupingBy { it }.eachCount() //do this against the StringBuilder to avoid a needless copy
+    }
+}
+
+internal fun parsePolymerTemplate(input: List<String>): PolymerTemplate {
+    return PolymerTemplate(input.first())
+}
+
+private  val pairInsertionRuleRegex = """(\w\w) -> (\w)""".toRegex()
+internal fun parsePairInsertionRules(input: List<String>): PairInsertionRules {
+    return input
+        .mapNotNull { pairInsertionRuleRegex.matchEntire(it) }
+        .map { it.destructured }
+        .associate { (polymerPair, polymerInsertion) -> PolymerPair(polymerPair) to PolymerInsertion(polymerInsertion.first()) }
 }
--- src/test/kotlin/day14/Day14Test.kt
+++ src/test/kotlin/day14/Day14Test.kt
@@ -1,4 +1,141 @@
 package day14
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
 class Day14Test {
+    private val sampleInput = """
+        NNCB
+
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
+    """.trimIndent().lines()
+    
+    @Test
+    fun `parsePolymerTemplate should return the polymer template from the input`() {
+        val polymerTemplate : PolymerTemplate = parsePolymerTemplate(sampleInput)
+
+        assert(polymerTemplate.toString() == "NNCB")
+    }
+
+    @Test
+    fun `parsePairInsertionRules should return a set of pair insertion rules`() {
+        val pairInsertionRules: PairInsertionRules = parsePairInsertionRules(sampleInput)
+
+        assert(pairInsertionRules.size == 16)
+        assert(pairInsertionRules[PolymerPair("CH")] == PolymerInsertion('B'))
+        assert(pairInsertionRules[PolymerPair("HH")] == PolymerInsertion('N'))
+        assert(pairInsertionRules[PolymerPair("CB")] == PolymerInsertion('H'))
+        assert(pairInsertionRules[PolymerPair("NH")] == PolymerInsertion('C'))
+        assert(pairInsertionRules[PolymerPair("HB")] == PolymerInsertion('C'))
+        assert(pairInsertionRules[PolymerPair("HC")] == PolymerInsertion('B'))
+        assert(pairInsertionRules[PolymerPair("HN")] == PolymerInsertion('C'))
+        assert(pairInsertionRules[PolymerPair("NN")] == PolymerInsertion('C'))
+        assert(pairInsertionRules[PolymerPair("BH")] == PolymerInsertion('H'))
+        assert(pairInsertionRules[PolymerPair("NC")] == PolymerInsertion('B'))
+        assert(pairInsertionRules[PolymerPair("NB")] == PolymerInsertion('B'))
+        assert(pairInsertionRules[PolymerPair("BN")] == PolymerInsertion('B'))
+        assert(pairInsertionRules[PolymerPair("BB")] == PolymerInsertion('N'))
+        assert(pairInsertionRules[PolymerPair("BC")] == PolymerInsertion('B'))
+        assert(pairInsertionRules[PolymerPair("CC")] == PolymerInsertion('N'))
+        assert(pairInsertionRules[PolymerPair("CN")] == PolymerInsertion('C'))
+    }
+
+    @Nested
+    inner class PolymerTemplateTest {
+
+        @Test
+        fun `step should simultaneously process all pair insertion rules that match sections in the polymer`() {
+            val polymerTemplate = parsePolymerTemplate(sampleInput)
+            val pairInsertionRules = parsePairInsertionRules(sampleInput)
+
+            polymerTemplate.step(pairInsertionRules)
+
+            assert(polymerTemplate.toString() == "NCNBCHB")
+        }
+
+        @Test
+        fun `step should not consider new pairs created in a step during that step`() {
+            val polymerTemplate = PolymerTemplate("NN")
+            val rules = mapOf(
+                PolymerPair("NN") to PolymerInsertion('C'),
+                PolymerPair("CN") to PolymerInsertion('X') //this rule must NOT fire
+            )
+
+            polymerTemplate.step(rules)
+
+            assert(!polymerTemplate.toString().contains('X'))
+        }
+
+        @Test
+        fun `step should apply each rule multiple times in a single invocation where necessary`() {
+            val polymerTemplate = PolymerTemplate("NNN")
+            val rules = mapOf(
+                PolymerPair("NN") to PolymerInsertion('C')
+            )
+
+            polymerTemplate.step(rules)
+
+            assert(polymerTemplate.toString() == "NCNCN")
+        }
+
+        @Test
+        fun `step should return expected results after 2 invocations`() {
+            val polymerTemplate = parsePolymerTemplate(sampleInput)
+            val rules = parsePairInsertionRules(sampleInput)
+
+            (1..2).forEach { _ -> polymerTemplate.step(rules) }
+
+            assert(polymerTemplate.toString() == "NBCCNBBBCBHCB")
+        }
+
+        @Test
+        fun `step should return expected results after 3 invocations`() {
+            val polymerTemplate = parsePolymerTemplate(sampleInput)
+            val rules = parsePairInsertionRules(sampleInput)
+
+            (1..3).forEach { _ -> polymerTemplate.step(rules) }
+
+            assert(polymerTemplate.toString() == "NBBBCNCCNBBNBNBBCHBHHBCHB")
+        }
+
+        @Test
+        fun `step should return expected results after 4 invocations`() {
+            val polymerTemplate = parsePolymerTemplate(sampleInput)
+            val rules = parsePairInsertionRules(sampleInput)
+
+            (1..4).forEach { _ -> polymerTemplate.step(rules) }
+
+            assert(polymerTemplate.toString() == "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")
+        }
+
+        @Test
+        fun `step should produce a polymer of the expected length after 10 invocations`() {
+            val polymerTemplate = parsePolymerTemplate(sampleInput)
+            val rules = parsePairInsertionRules(sampleInput)
+
+            (1..10).forEach { _ -> polymerTemplate.step(rules) }
+
+            assert(polymerTemplate.toString().length == 3073)
+        }
+    }
+
+    @Test
+    fun `part1 should process the polymer for 10 steps, find the elements that occur most and least frequently, and return the difference of their counts`() {
+        assert(part1(sampleInput) == 1588)
+    }
 }
