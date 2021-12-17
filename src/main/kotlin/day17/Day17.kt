09dbf82348a1cd4b6258eac47cb120726e991e9a
Sean Reilly <seanjreilly@gmail.com>
2021-12-17 15:53:39 +0000

    Code cleanup
    Extract duplicate code into calculateMinPossibleXVelocity function

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -1,10 +1,10 @@
-    package day17
+package day17
 
 import utils.readInput
 import kotlin.math.abs
 import kotlin.math.sign
 
-    fun main() {
+fun main() {
     val input = readInput("Day17")
     println(part1(input.first()))
     println(part2(input.first()))
@@ -13,13 +13,8 @@ import kotlin.math.sign
 fun part1(input: String): Long {
     val target = TargetArea(input)
 
-    //min possible x = min value where findFarthestX is past the edge of the target
-    val minPossibleXVelocity: Int
-    var candidateMinX = 0
-    while (findFarthestX(candidateMinX) < target.targetXPosition.first) {
-        candidateMinX++
-    }
-    minPossibleXVelocity = candidateMinX
+    //minimum possible x give the highest possible y
+    val minPossibleXVelocity: Int = calculateMinPossibleXVelocity(target)
 
     // every possible velocity encountered on the way up is encountered in reverse on the way down,
     // and therefore the probe will *always* hit y = 0, having moved -initalY to get there.
@@ -36,23 +31,30 @@ fun part1(input: String): Long {
 fun part2(input: String): Int {
     val target = TargetArea(input)
 
-    //min possible x = min value where findFarthestX is past the edge of the target
-    val minPossibleXVelocity: Int
-    var candidateMinX = 0
-    while (findFarthestX(candidateMinX) < target.targetXPosition.first) {
-        candidateMinX++
-    }
-    minPossibleXVelocity = candidateMinX
+    val minPossibleXVelocity = calculateMinPossibleXVelocity(target)
     val maxPossibleXVelocity = target.targetXPosition.last() //any higher, and the probe is guaranteed to shoot past in one step
-
     val minPossibleYVelocity = target.targetYPosition.first //any lower, and the probe is guaranteed to shoot below in one step
     //we know from part 1 that any velocity over abs(target.minY) - 1 will miss the window on the way down
     val maxPossibleYVelocity = abs(target.targetYPosition.first) - 1
 
     val potentialInputVelocities = (minPossibleXVelocity..maxPossibleXVelocity)
-        .flatMap { x -> (minPossibleYVelocity..maxPossibleYVelocity).map {y -> Pair(x,y) } }
+        .flatMap { x -> (minPossibleYVelocity..maxPossibleYVelocity).map { y -> Pair(x, y) } }
 
     return potentialInputVelocities.count { willHitTarget(target, it.first, it.second) }
+}
+
+/*
+The minimum x velocity that can hit a target is always
+the smallest value where firstFarthestX is within the target
+ */
+private fun calculateMinPossibleXVelocity(target: TargetArea): Int {
+    val minPossibleXVelocity: Int
+    var candidateMinX = 0
+    while (findFarthestX(candidateMinX) !in target.targetXPosition) {
+        candidateMinX++
+    }
+    minPossibleXVelocity = candidateMinX
+    return minPossibleXVelocity
 }
 
 internal data class Point(val x: Int, val y: Int)
@@ -64,12 +66,13 @@ internal data class TargetArea(val targetXPosition: IntRange, val targetYPositio
 }
 
 @Suppress("TestFunctionName") //fake constructor
-internal fun TargetArea(input: String) : TargetArea {
+internal fun TargetArea(input: String): TargetArea {
     val (x1, x2, y1, y2) = inputRegex.matchEntire(input)!!.destructured
     val xRange = x1.toInt()..x2.toInt()
     val yRange = y1.toInt()..y2.toInt()
     return TargetArea(xRange, yRange)
 }
+
 private val inputRegex = """target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)""".toRegex()
 
 
@@ -99,10 +102,11 @@ internal fun findMaximumY(initialY: Int): Long {
         else -> (1L..initialY.toLong()).sum()
     }
 }
+
 internal fun findFarthestX(initialX: Int): Int {
     return when {
         initialX == 0 -> 0
         initialX > 0 -> (1..initialX).sum()
-        else  -> (initialX.. -1).sum()
+        else -> (initialX..-1).sum()
     }
 }
