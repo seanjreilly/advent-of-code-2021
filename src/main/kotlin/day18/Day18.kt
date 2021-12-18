93a7e70c7be7b910bea0a90ddc2f597e8fb69b02
Sean Reilly <seanjreilly@gmail.com>
2021-12-18 10:09:30 +0000

    Add template for day 18

---
+++ src/main/kotlin/day18/Day18.kt
@@ -0,0 +1,17 @@
+package day18
+
+import utils.readInput
+
+fun main() {
+    val input = readInput("Day18")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
