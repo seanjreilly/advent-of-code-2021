d8ba004a4b7ba2e2fb7fd937088153d74f13a11b
Sean Reilly <seanjreilly@gmail.com>
2021-12-20 21:57:11 +0000

    Work in progress for part 1
    A solution that passes all tests but doesn't work on real input.
    Not sure why yet.

--- src/main/kotlin/day20/Day20.kt
+++ src/main/kotlin/day20/Day20.kt
@@ -1,5 +1,6 @@
 package day20
 
+import utils.gridmap.Point
 import utils.readInput
 
 fun main() {
@@ -9,9 +10,107 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val enhancementAlgorithm = parseEnhancementAlgorithm(input)
+    val image = Image(input)
+    val stage1 = image.enhance(enhancementAlgorithm)
+    val result = stage1.enhance(enhancementAlgorithm)
+    return result.countLitPixels()
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+internal fun parseEnhancementAlgorithm(input: List<String>): BooleanArray {
+    return input.first().toCharArray().map { it.toBinary() }.toBooleanArray()
+}
+
+internal data class Image(private val setPoints: Set<Point>, val width:Int, val height: Int, val backgroundColor: Boolean = false) : Iterable<Point> {
+
+    internal operator fun get(point: Point): Boolean = point in setPoints
+
+    fun countLitPixels() : Int {
+        if (backgroundColor) {
+            throw IllegalStateException("infinite number of set pixels")
+        }
+        return setPoints.size
+    }
+
+    override fun iterator() = iterator {
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                yield(Point(x, y))
+            }
+        }
+    }
+
+    fun enhance(enhancementAlgorithm: BooleanArray): Image {
+        val newSetPoints = (-1 until width).flatMap { x ->
+            (-1 until height).mapNotNull { y ->
+                val point = Point(x, y)
+                val grid = point.threeByThreeSquare()
+                val codeForGrid = grid.map {
+                    when {
+                        isOutOfBounds(it) -> false
+                        else -> (it in setPoints)
+                    }
+                }
+                .map { it.toBinary() }
+                .joinToString("")
+                .toInt(2)
+
+                val enhancedResult = enhancementAlgorithm[codeForGrid]
+                if (enhancedResult) point else null
+            }
+            //shift the negative -1 row and -1 column to the new 0 row and 0 column
+            .map { Point(it.x + 1,it.y + 1) }
+        }.toSet()
+        val newWidth = width + 2
+        val newHeight = height + 2
+        val newBackgroundColor = if (backgroundColor) enhancementAlgorithm["111111111".toInt(2)] else enhancementAlgorithm[0]
+        return Image(newSetPoints, newWidth, newHeight, newBackgroundColor)
+    }
+
+    private fun isOutOfBounds(point: Point) : Boolean {
+        return (point.x !in (0 until width)) || (point.y !in (0 until height))
+    }
+}
+
+internal fun Image(input: List<String>) : Image {
+    val imagePortionOfInput = input.drop(2)
+
+    val width = imagePortionOfInput.first().length
+    val height = imagePortionOfInput.size
+
+    val setPoints = imagePortionOfInput
+        .flatMapIndexed { y, line ->
+            check(line.length == width) { "image must be rectangular" }
+            line.toCharArray().mapIndexed { x, char ->
+                val point = Point(x, y)
+                Pair(char.toBinary(), point)
+            }
+        }
+        .mapNotNull { if (it.first) it.second else null }
+        .toSet()
+
+    return Image(setPoints, width, height, false)
+}
+
+private fun Char.toBinary() = when (this) {
+    '#' -> true
+    '.' -> false
+    else -> throw IllegalArgumentException("unexpected character '$this'")
+}
+
+private fun Boolean.toBinary() = when(this) {
+    true -> '1'
+    false -> '0'
+}
+
+private fun Point.threeByThreeSquare() : List<Point> {
+    return listOf(
+        this.northWest(), this.north(), this.northEast(),
+        this.west(), this, this.east(),
+        this.southWest(), this.south(), this.southEast()
+    )
 }
--- src/test/kotlin/day20/Day20Test.kt
+++ src/test/kotlin/day20/Day20Test.kt
@@ -1,4 +1,221 @@
 package day20
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import utils.gridmap.Point
+import java.lang.IllegalStateException
+
 class Day20Test {
+
+    //a one-line piece of input broken into multiple lines for sanity
+    private val sampleEnhancementAlgorithm = """..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##
+#..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###
+.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#.
+.#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#.....
+.#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#..
+...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....
+..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#""".replace("\n","")
+
+    private val sampleInput = """
+        ${sampleEnhancementAlgorithm}
+        
+        ...............
+        ...............
+        ...............
+        ...............
+        ...............
+        .....#..#......
+        .....#.........
+        .....##..#.....
+        .......#.......
+        .......###.....
+        ...............
+        ...............
+        ...............
+        ...............
+        ...............
+    """.trimIndent().lines()
+
+    //had to add extra edges myself because the test input doesn't do this
+    val expectedStageOneResult = """
+        .................
+        .................
+        .................
+        .................
+        .................
+        ......##.##......
+        .....#..#.#......
+        .....##.#..#.....
+        .....####..#.....
+        ......#..##......
+        .......##..#.....
+        ........#.#......
+        .................
+        .................
+        .................
+        .................
+        .................
+    """.trimIndent().lines()
+
+    //had to add extra edges myself because the test input doesn't do this
+    val expectedStageTwoResult = """
+        ...................
+        ...................
+        ...................
+        ...................
+        ...................
+        ............#......
+        ......#..#.#.......
+        .....#.#...###.....
+        .....#...##.#......
+        .....#.....#.#.....
+        ......#.#####......
+        .......#.#####.....
+        ........##.##......
+        .........###.......
+        ...................
+        ...................
+        ...................
+        ...................
+        ...................
+    """.trimIndent().lines()
+
+    @Test
+    fun `parseEnhancementAlgorithm should return a boolean array given a string of hashes and dots`() {
+        val result: BooleanArray = parseEnhancementAlgorithm(sampleInput)
+
+        assert(result.size == 512)
+        assert(!result[0])
+        assert(!result[1])
+        assert(result[2])
+        assert(!result[509])
+        assert(!result[510])
+        assert(result[511])
+    }
+
+    @Nested
+    inner class ImageTest {
+        @Test
+        fun `constructor should return a 15x15 grid with a background color of 0 given sample input`() {
+            val image = Image(sampleInput)
+
+            assert(image.width == 15)
+            assert(image.height == 15)
+            assert(!image.backgroundColor)
+        }
+
+        @Test
+        fun `enhance should return a new larger image using the image enhancement algorithm`() {
+            val enhancementAlgorithm = parseEnhancementAlgorithm(sampleInput)
+            val image = Image(sampleInput)
+            val expectedResult = Image(listOf("garbage line", "garbage line") + expectedStageOneResult)
+
+            val result: Image = image.enhance(enhancementAlgorithm)
+
+            //image is 2 higher and 2 wider because pixels one beyond the border can be set if the puzzle is sneaky
+            //(and I've checked. It's sneaky)
+
+            assert(result.width == image.width + 2)
+            assert(result.height == image.height + 2)
+            assert(result == expectedResult)
+        }
+
+        @Test
+        fun `enhance should work multiple times`() {
+            val enhancementAlgorithm = parseEnhancementAlgorithm(sampleInput)
+            val image = Image(sampleInput)
+            val expectedResult = Image(listOf("garbage line", "garbage line") + expectedStageTwoResult)
+
+            val result: Image = image.enhance(enhancementAlgorithm).enhance(enhancementAlgorithm)
+
+            assert(result.countLitPixels() == 35)
+            assert(result.width == image.width + 4)
+            assert(result.height == image.height + 4)
+            assert(result == expectedResult)
+        }
+
+        @Test
+        fun `enhance should work correctly when images off the edge of the current image are set`() {
+            /*
+            the bit at index 1 (corresponding to the bottom corner) is set in the enhancement algorithm
+            the top left bit (only) of the image is set
+            this combination should set a point outside of the original image
+            which will become the new point 0,0
+
+            everything else in the enhanced image should be unset
+            */
+            val enhancementAlgorithm =  parseEnhancementAlgorithm(listOf("." + "#" + ".".repeat(510)))
+            val imageSource = """
+                ignore this line
+                
+                #..
+                ...
+                ...
+            """.trimIndent().lines()
+            val image = Image(imageSource)
+
+            val result = image.enhance(enhancementAlgorithm)
+
+            assert(result.countLitPixels() == 1)
+            assert(result[Point(0,0)])
+        }
+
+        @Test
+        fun `enhance should set the backgroundColor if the test input is really sneaky and turns all zeros into ones`() {
+            //the sample grid doesn't do this but the actual input does!
+            val enhancementAlgorithm =  parseEnhancementAlgorithm(listOf("#".repeat(512)))
+            val imageSource = """
+                ignore this line
+                
+                ...
+                ...
+                ...
+            """.trimIndent().lines()
+            val image = Image(imageSource)
+
+            val result = image.enhance(enhancementAlgorithm)
+
+            assert(result.backgroundColor)
+        }
+
+        @Test
+        fun `enhance should switch the background color back if unset by the mask`() {
+            val enhancementAlgorithm =  parseEnhancementAlgorithm(listOf("#".repeat(511) + '.'))
+            val image = Image(emptySet(), 5, 5, true)
+
+            val result = image.enhance(enhancementAlgorithm)
+
+            assert(!result.backgroundColor)
+        }
+
+        @Test
+        fun `countLitPixels should return the number of lit pixels given an image with an unset background`() {
+            val imageSource = """
+                ignore this line
+                
+                ...
+                .#.
+                ...
+            """.trimIndent().lines()
+            val image = Image(imageSource)
+
+            val result = image.countLitPixels()
+
+            assert(result == 1)
+        }
+
+        @Test
+        fun `countLitPixels should throw an exception given an image with set infinite background`() {
+            //as the number of lit pixels would be infinity
+            val image = Image(emptySet(), 1, 1, backgroundColor = true)
+
+            assertThrows<IllegalStateException> { image.countLitPixels() }
+        }
+    }
+
+    @Test
+    fun `part1 should parse an image and enhancement algorithm, enhance the image twice, and return the number of set pixels in the twice-enhanced image`() {
+        assert(part1(sampleInput) == 35)
+    }
 }
