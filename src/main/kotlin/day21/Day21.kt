be57f02fb8c3410e0b531f23d500bc619f26efa6
Sean Reilly <seanjreilly@gmail.com>
2021-12-21 12:51:22 +0000

    Add skeleton for day 21

---
+++ src/main/kotlin/day21/Day21.kt
@@ -0,0 +1,17 @@
+package day21
+
+import utils.readInput
+
+fun main() {
+    val input = readInput("Day21")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
