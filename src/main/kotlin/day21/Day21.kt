e15fa5b7045a5836eec24cec056e2b2b1e95445b
Sean Reilly <seanjreilly@gmail.com>
2021-12-21 13:37:04 +0000

    Day 21, Part 1
    Implemented the game in part1

--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -1,6 +1,7 @@
 package day21
 
 import utils.readInput
+import java.util.*
 
 fun main() {
     val input = readInput("Day21")
@@ -9,9 +10,60 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val players = parse(input)
+    val die = DeterministicDie()
+    while (true) {
+        //alternate players by taking the player from the head of the queue and adding them back to the tail
+        val player = players.remove()
+        players.add(player)
+
+        player.score += player.advance(die.roll(3)) + 1 //zero-based position so we need to add 1 to the score
+        if (player.score >= 1000) {
+            //we have a winner
+            val losingPlayersScore = players.first.score
+            return losingPlayersScore * die.timesRolled
+        }
+    }
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+internal class DeterministicDie {
+    internal var timesRolled: Int = 0
+        private set
+
+    private val numbers = iterator {
+        while (true) {
+            for (i in 1..100) {
+                timesRolled++
+                yield(i)
+            }
+        }
+    }
+
+    fun roll(times: Int): Int {
+        var result = 0
+        for (i in 1..times) {
+            result += numbers.next()
+        }
+        return result
+    }
+}
+
+internal fun parse(input: List<String>): LinkedList<Player> {
+    val players = input
+        .map { it.toCharArray().last().toString().toInt() - 1 }
+        .map { Player(it, 0) }
+        .toList()
+
+    return LinkedList(players)
+}
+
+internal data class Player(var position: Int, var score: Int) {
+    fun advance(spaces: Int) : Int {
+        position = (position + spaces) % 10
+        return position
+    }
 }
--- src/test/kotlin/day21/Day21Test.kt
+++ src/test/kotlin/day21/Day21Test.kt
@@ -1,4 +1,76 @@
 package day21
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.util.*
+
 class Day21Test {
+    val sampleInput = """
+        Player 1 starting position: 4
+        Player 2 starting position: 8
+    """.trimIndent().lines()
+
+    @Nested
+    inner class DeterministicDieTest {
+        @Test
+        fun `calling roll 101 times should return in the numbers 1 to 100 and then 1 again`() {
+            val die = DeterministicDie()
+
+            val expectedResult = (1..100).toList() + 1
+
+            val result = (1..101)
+                .map { die.roll(1) }
+                .toList()
+
+            assert(result == expectedResult)
+        }
+
+        @Test
+        fun `timesRolled should return 0 if roll hasn't been called`() {
+            val die = DeterministicDie()
+
+            assert(die.timesRolled == 0)
+        }
+
+        @Test
+        fun `timesRolled should increase when roll is called`() {
+            val die = DeterministicDie()
+
+            (1..1000).forEach {
+                die.roll(1)
+                assert(die.timesRolled == it)
+            }
+        }
+
+        @Test
+        fun `roll should return the sum of calling roll three times given 3`() {
+            val die = DeterministicDie()
+
+            val result = die.roll(3)
+
+            assert(result == 6)
+            assert(die.timesRolled == 3)
+
+            val secondResult = die.roll(3)
+
+            assert(secondResult == 15)
+            assert(die.timesRolled == 6)
+        }
+    }
+
+    @Test
+    fun `parse should return a LinkedList of players`() {
+        val players: LinkedList<Player> = parse(sampleInput)
+
+        assert(players.size == 2)
+        assert(players[0].position == 3) //use zero-based positions instead of one-based
+        assert(players[0].score == 0)
+        assert(players[1].position == 7) //use zero-based positions instead of one-based
+        assert(players[1].score == 0)
+    }
+
+    @Test
+    fun `part1 should parse input, play until the first player's score is 1000 or higher, and then return the losing players turn multiplied the number of times the die was rolled`() {
+        assert(part1(sampleInput) == 739785)
+    }
 }
