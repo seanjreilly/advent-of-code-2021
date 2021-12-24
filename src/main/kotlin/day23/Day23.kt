28579a5380f41465dcad01c8bc3248799fc5a916
Sean Reilly <seanjreilly@gmail.com>
2021-12-24 01:22:56 +0000

    Implement the primitives need to solve day 23, part 1
    Build a SpaceMap that captures where amphipods can go, and a
    position object that captures where they are, and builds
    potential next steps.

--- src/main/kotlin/day23/Day23.kt
+++ src/main/kotlin/day23/Day23.kt
@@ -15,3 +15,197 @@ fun part1(input: List<String>): Int {
 fun part2(input: List<String>): Int {
     return input.size
 }
+
+internal enum class AmphipodType(val movementCost: Int) {
+    Amber(1),
+    Bronze(10),
+    Copper(100),
+    Desert(1000);
+}
+
+internal data class Amphipod(val id: Int, val type: AmphipodType)
+
+internal sealed class Space {
+    abstract val id: Int
+    lateinit var moves: Set<PotentialMove>
+}
+internal data class RoomSpace(override val id: Int, val type: AmphipodType) : Space()
+internal data class HallwaySpace(override val id: Int): Space()
+
+internal data class PotentialMove(val source: Space, val intermediateSpaces: Set<Space>, val destination: Space) {
+    init {
+        require(source != destination) { "cannot build a route from a space to itself" }
+        if (source is RoomSpace) {
+            if (destination is RoomSpace) {
+                require(source.type != destination.type) { "cannot build a route from a room to another room of the same type" }
+            } else {
+                require(intermediateSpaces.count { it is HallwaySpace } > 0) { "amphipods only stop in hallway spaces that don't block rooms" }
+            }
+        }
+        if (source is HallwaySpace) {
+            require(destination !is HallwaySpace) { "amphipods only stop in rooms once they are in a hallway" }
+        }
+    }
+
+    val distance: Int = intermediateSpaces.size + 1
+}
+
+// a big ugly map of spaces and how they connect to each other, directly and transitively
+// encodes rules like amphipods don't move from hallway to hallway
+internal object SpaceMap {
+    val hallwaySpaces: List<HallwaySpace> = listOf(
+        HallwaySpace(1), //leftmost space
+        HallwaySpace(2), //left wing
+        HallwaySpace(4), //between room a and room b
+        HallwaySpace(6), //between room b and room c
+        HallwaySpace(8), //between room c and room d
+        HallwaySpace(10), //right wing
+        HallwaySpace(11), //rightmost space
+    )
+    //the hallway spaces where amphipods won't stop
+    val intermediateHallwaySpaces: List<HallwaySpace> = listOf(
+        HallwaySpace(3), //above room a
+        HallwaySpace(5), //above room b
+        HallwaySpace(7), //above room c
+        HallwaySpace(9), //above room d
+    )
+    val roomSpaces: List<RoomSpace> = listOf(
+        RoomSpace(12, AmphipodType.Amber), //top room
+        RoomSpace(13, AmphipodType.Amber), //bottom room
+        RoomSpace(14, AmphipodType.Bronze), //top room
+        RoomSpace(15, AmphipodType.Bronze), //bottom room
+        RoomSpace(16, AmphipodType.Copper), //top room
+        RoomSpace(17, AmphipodType.Copper), //bottom room
+        RoomSpace(18, AmphipodType.Desert), //top room
+        RoomSpace(19, AmphipodType.Desert), //bottom room
+    )
+
+    val roomSpacesByType = roomSpaces
+        .groupBy { it.type }
+        .mapValues { it.value.toSet() }
+
+    //build a mini-graph to build the total routes between the points we need
+    private val neighbours = mapOf(
+        1 to listOf(2),
+        2 to listOf(1, 3),
+        3 to listOf(2, 4, 12),
+        4 to listOf(3, 5),
+        5 to listOf(4, 6, 14),
+        6 to listOf(5, 7),
+        7 to listOf(6, 8, 16),
+        8 to listOf(7, 9),
+        9 to listOf(8, 10, 18),
+        10 to listOf(9, 11),
+        11 to listOf(10),
+        12 to listOf(3, 13),
+        13 to listOf(12),
+        14 to listOf(5, 15),
+        15 to listOf(14),
+        16 to listOf(7, 17),
+        17 to listOf(16),
+        18 to listOf(9, 19),
+        19 to listOf(18),
+    )
+
+    private fun findRouteFrom(path:List<Int>, destination: Int): List<Int>? {
+        if (path.last() == destination) { return path }
+        val nextSteps = neighbours[path.last()]!!
+            .filter { it !in path }
+
+        if (nextSteps.isEmpty()) { return null }
+
+        return nextSteps
+            .map { path + it }
+            .firstNotNullOfOrNull { findRouteFrom(it, destination) }
+    }
+
+    init {
+        intermediateHallwaySpaces.forEach { it.moves = emptySet() }
+
+        val allSpaces = (roomSpaces + hallwaySpaces + intermediateHallwaySpaces).associateBy { it.id }
+
+        fun buildMove(from: Space, to:Space): PotentialMove {
+            val path = findRouteFrom(listOf(from.id), to.id)!!
+            val intermediates = path.drop(1).dropLast(1).map { allSpaces[it]!! }.toSet()
+            return PotentialMove(from, intermediates, to)
+        }
+
+        hallwaySpaces.forEach { space ->
+            space.moves = roomSpaces.map { buildMove(space, it) }.toSet()
+        }
+
+        roomSpaces.forEach { space ->
+            val routesToHallways = hallwaySpaces.map { buildMove(space, it) }
+            val routesToOtherRooms = roomSpaces.filter { it.type != space.type }.map { buildMove(space, it) }
+            space.moves = (routesToHallways + routesToOtherRooms).toSet()
+        }
+    }
+}
+
+internal data class Position(val locations:Map<Amphipod, Space>) {
+    internal val reverseMap = locations.entries.map { it.value to it.key }.toMap()
+
+    fun nextMoves(): List<Pair<Position, Int>> {
+        return locations.flatMap { (amphipod, space) -> //for each amphipod
+            space
+                .moves //all moves reachable from the amphipod's location
+                .filter { !isBlocked(it) } //that aren't occupied or blocked
+                .filter { canGo(amphipod, it.destination) } //that the amphipod wants to visit
+                .map {
+                    val newLocations = locations.toMutableMap()
+                    newLocations[amphipod] = it.destination
+                    Pair(Position(newLocations), it.distance * amphipod.type.movementCost)
+                }
+        }
+    }
+
+    fun canGo(amphipod: Amphipod, destination: Space) : Boolean {
+        if (destination !is RoomSpace) {
+            return true
+        }
+        if (amphipod.type != destination.type) {
+            return false
+        }
+        val amphipodsInRoom = reverseMap.keys.intersect(SpaceMap.roomSpacesByType[destination.type]!!).map { reverseMap[it]!! }
+        if (amphipodsInRoom.count { it.type != destination.type } > 0) {
+            return false //bad amphipods in the room
+        }
+        return true
+    }
+
+    fun isBlocked(move: PotentialMove): Boolean {
+        return move.destination in reverseMap.keys || reverseMap.keys.intersect(move.intermediateSpaces).isNotEmpty()
+    }
+
+    fun isFinished(): Boolean {
+        SpaceMap.roomSpacesByType
+            .flatMap { (key, value) -> value.map { Pair(key, it) } }
+            .forEach { (type, space) ->
+                val occupant = reverseMap[space]
+                if (occupant == null || occupant.type != type) {
+                    return false
+                }
+            }
+        return true
+    }
+}
+
+private val parseRegex = """.{1,3}?(\w).(\w).(\w).(\w).{1,3}""".toRegex()
+internal fun parse(input: List<String>): Position {
+    fun parseType(firstLetter:String): AmphipodType {
+        return AmphipodType.values().find { it.name.startsWith(firstLetter) }!!
+    }
+    val (a1, b1, c1, d1) = parseRegex.matchEntire(input[2])!!.destructured
+    val (a2, b2, c2, d2) = parseRegex.matchEntire(input[3])!!.destructured
+
+    return Position(mapOf(
+        Amphipod(1, parseType(a1)) to SpaceMap.roomSpaces[0],
+        Amphipod(3, parseType(b1)) to SpaceMap.roomSpaces[2],
+        Amphipod(5, parseType(c1)) to SpaceMap.roomSpaces[4],
+        Amphipod(7, parseType(d1)) to SpaceMap.roomSpaces[6],
+        Amphipod(2, parseType(a2)) to SpaceMap.roomSpaces[1],
+        Amphipod(4, parseType(b2)) to SpaceMap.roomSpaces[3],
+        Amphipod(6, parseType(c2)) to SpaceMap.roomSpaces[5],
+        Amphipod(8, parseType(d2)) to SpaceMap.roomSpaces[7],
+    ))
+}
--- src/test/kotlin/day23/Day23Test.kt
+++ src/test/kotlin/day23/Day23Test.kt
@@ -1,9 +1,391 @@
 package day23
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
 class Day23Test {
+    @Nested
+    inner class AmphipodTypeTest {
+        @Test
+        fun `values should return 4 types of amphipod`() {
+            val types:Array<AmphipodType> = AmphipodType.values()
 
+            assert(types.size == 4)
+            assert(AmphipodType.Amber in types)
+            assert(AmphipodType.Bronze in types)
+            assert(AmphipodType.Copper in types)
+            assert(AmphipodType.Desert in types)
+        }
+
+        @Test
+        fun `each amphipod type has a movement cost`() {
+            assert(AmphipodType.Amber.movementCost == 1)
+            assert(AmphipodType.Bronze.movementCost == 10)
+            assert(AmphipodType.Copper.movementCost == 100)
+            assert(AmphipodType.Desert.movementCost == 1000)
+        }
+
+        @Test
+        fun `no two ampipod types have the same movement cost`() {
+            val movementCostCounts = AmphipodType.values()
+                .groupingBy { it.movementCost }
+                .eachCount()
+
+            assert(movementCostCounts.maxOf { it.value } == 1)
+        }
+    }
+
+    @Nested
+    inner class AmphipodTest {
+        @Test
+        fun `Amphipods have a type and an id`() {
+            val expectedType = AmphipodType.Copper
+            val expectedId = 1
+
+            val amphipod = Amphipod(expectedId, expectedType)
+
+            assert(amphipod.id == expectedId)
+            assert(amphipod.type == expectedType)
+        }
+    }
+
+    internal abstract inner class SpaceTest<T:Space> {
+        protected abstract val instance:T
+
+        @Test
+        fun `has an id`() {
+            assert(instance.id > 0)
+        }
+
+        @Test
+        fun `has a set of potential moves`() {
+            val moves = getAtLeastOneValidMoveForSpace()
+
+            instance.moves = moves
+
+            assert(instance.moves == moves)
+        }
+
+        protected abstract fun getAtLeastOneValidMoveForSpace(): Set<PotentialMove>
+    }
+
+    @Nested
+    internal inner class RoomSpaceTest : SpaceTest<RoomSpace>() {
+        override val instance = RoomSpace(1, AmphipodType.Bronze)
+
+        @Test
+        fun `type returns the type of the RoomSpace`() {
+            assert(instance.type == AmphipodType.Bronze)
+        }
+
+        override fun getAtLeastOneValidMoveForSpace(): Set<PotentialMove> {
+            //up and around the corner
+            val destination = HallwaySpace(3)
+            val intermediate = HallwaySpace(2)
+            return setOf(PotentialMove(instance, setOf(intermediate), destination))
+        }
+    }
+
+    @Nested
+    internal inner class HallwaySpaceTest : SpaceTest<HallwaySpace>() {
+        override val instance = HallwaySpace(12)
+        override fun getAtLeastOneValidMoveForSpace(): Set<PotentialMove> {
+            //across and down into a room
+            val destination = RoomSpace(1, AmphipodType.Amber)
+            val intermediateSpaces = setOf(
+                RoomSpace(2, AmphipodType.Amber), //other space in the same room
+                HallwaySpace(6), //space just above the room
+                HallwaySpace(7), //other hallway space
+            )
+            return setOf(PotentialMove(instance, intermediateSpaces, destination))
+        }
+    }
+
+    @Nested
+    inner class PotentialMoveTest {
+        @Test
+        fun `PotentialMove has a source space, a destination space, and a set of zero or more intermediate spaces`() {
+            //model a route from the bottom of a room up and just around a corner
+            val source:Space = RoomSpace(1, AmphipodType.Desert)
+            val destination:Space = HallwaySpace(4)
+            val intermediateSpaces = setOf(RoomSpace(2, AmphipodType.Desert), HallwaySpace(3))
+
+            val potentialMove = PotentialMove(source, intermediateSpaces, destination)
+
+            assert(potentialMove.source == source)
+            assert(potentialMove.destination == destination)
+            assert(potentialMove.intermediateSpaces == intermediateSpaces)
+        }
+
+        @Test
+        fun `constructor should fail given a route from a space to itself`() {
+            val source:Space = RoomSpace(1, AmphipodType.Desert)
+            val intermediateSpaces = setOf(HallwaySpace(2))
+
+            val expectedException = assertThrows<IllegalArgumentException> { PotentialMove(source, intermediateSpaces, source) }
+
+            assert("cannot build a route from a space to itself" in expectedException.message!!)
+        }
+
+        @Test
+        fun `constructor should fail given a route from a room space to another room space of the same type`() {
+            val source:Space = RoomSpace(1, AmphipodType.Desert)
+            val destination:Space = RoomSpace(2, AmphipodType.Desert)
+
+            val expectedException = assertThrows<IllegalArgumentException> { PotentialMove(source, emptySet(), destination) }
+
+            assert("cannot build a route from a room to another room of the same type" in expectedException.message!!)
+        }
+
+        @Test
+        fun `constructor should fail given a route from a hallway space to another hallway space`() {
+            val source:Space = HallwaySpace(6)
+            val destination:Space = HallwaySpace(7)
+
+            val expectedException = assertThrows<IllegalArgumentException> { PotentialMove(source, emptySet(), destination) }
+
+            assert("amphipods only stop in rooms once they are in a hallway" in expectedException.message!!)
+        }
+
+        @Test
+        fun `constructor should fail given a route from a room space to a hallway space without at least one intermediate hallway space`() {
+            val source:Space = RoomSpace(6, AmphipodType.Amber)
+            val destination:Space = HallwaySpace(7)
+
+            val expectedException = assertThrows<IllegalArgumentException> { PotentialMove(source, emptySet(), destination) }
+
+            assert("amphipods only stop in hallway spaces that don't block rooms" in expectedException.message!!)
+        }
+
+        @Test
+        fun `constructor should fail given a route from a room space to a hallway space with only an intermediate room space`() {
+            //model moving from the bottom space of a room to just outside the door
+            val source:Space = RoomSpace(5, AmphipodType.Amber)
+            val destination:Space = HallwaySpace(7)
+            val intermediateSpace = RoomSpace(6, AmphipodType.Amber)
+
+            val expectedException = assertThrows<IllegalArgumentException> { PotentialMove(source, setOf(intermediateSpace), destination) }
+
+            assert("amphipods only stop in hallway spaces that don't block rooms" in expectedException.message!!)
+        }
+
+        @Test
+        fun `distance should return the number of intermediate points plus one`() {
+            val source:Space = RoomSpace(1, AmphipodType.Desert)
+            val destination:Space = HallwaySpace(4)
+            val intermediateSpaces = setOf(RoomSpace(2, AmphipodType.Desert), HallwaySpace(3))
+
+            val potentialMove = PotentialMove(source, intermediateSpaces, destination)
+
+            assert(potentialMove.distance == 3)
+        }
+    }
+
+    @Nested
+    inner class SpaceMapTest {
+
+        @Test
+        fun `map should have 7 hallway spaces`() {
+            val spaces: List<HallwaySpace> = SpaceMap.hallwaySpaces
+
+            assert(spaces.size == 7)
+            assert(spaces.toSet().size == 7) { "all hallway spaces are unique" }
+        }
+
+        @Test
+        fun `map should have 8 room spaces, 2 of each type`() {
+            val spaces: List<RoomSpace> = SpaceMap.roomSpaces
+
+            assert(spaces.size == 8)
+            assert(spaces.toSet().size == 8) { "all room spaces are unique" }
+
+            assert(spaces.count { it.type == AmphipodType.Amber } == 2)
+            assert(spaces.count { it.type == AmphipodType.Bronze } == 2)
+            assert(spaces.count { it.type == AmphipodType.Copper } == 2)
+            assert(spaces.count { it.type == AmphipodType.Desert } == 2)
+        }
+
+        @Test
+        fun `every space should have a different id`() {
+            val allSpaces: List<Space> = SpaceMap.hallwaySpaces + SpaceMap.roomSpaces + SpaceMap.intermediateHallwaySpaces
+
+            assert(allSpaces.groupingBy { it.id }.eachCount().maxOf { it.value } == 1)
+        }
+
+        @Test
+        fun `intermediate rooms should have no potential moves`() {
+            SpaceMap.intermediateHallwaySpaces.forEach {
+                assert(it.moves.isEmpty())
+            }
+        }
+
+        @Test
+        fun `every hallway space should have a journey to every room space`() {
+            SpaceMap.hallwaySpaces.forEach { hallwaySpace ->
+                SpaceMap.roomSpaces.forEach { roomSpace ->
+                    val moves = hallwaySpace.moves
+                    assert(moves.find { it.destination == roomSpace} != null)
+                }
+            }
+        }
+
+        @Test
+        fun `every room space should have a journey to every hallway space and every room space of a different type`() {
+            SpaceMap.roomSpaces.forEach { roomSpace ->
+                SpaceMap.hallwaySpaces.forEach { hallwaySpace ->
+                    val moves = roomSpace.moves
+                    val moveFromRoomToSpace = moves.find { it.destination == hallwaySpace }
+                    assert(moveFromRoomToSpace != null)
+                    assert(
+                        moveFromRoomToSpace == hallwaySpace.moves.find { it.destination == roomSpace }!!.reverse()
+                    ) { "room to hallway should pass through the same spaces as hallway to room" }
+                }
+
+                assert(roomSpace.moves.count { it.destination is RoomSpace } == 6)
+                SpaceMap.roomSpaces.filter { it.type != roomSpace.type }.forEach { otherRoomSpace ->
+                    assert(roomSpace.moves.any { it.destination == otherRoomSpace }) { "rooms must visit all other rooms" }
+                }
+            }
+        }
+    }
+
+    @Nested
+    inner class PositionTest {
+        @Test
+        fun `nextMoves should return the potential next moves from a given position`() {
+            val a = Amphipod(1, AmphipodType.Amber)
+            val amphipodLocations = mapOf(a to SpaceMap.hallwaySpaces[0])
+            val position = Position(amphipodLocations)
+
+            val nextMoves: List<Pair<Position, Int>> = position.nextMoves()
+
+            assert(nextMoves.size == 2) //should be able to move to the a positions only
+        }
+
+        @Test
+        fun `isBlocked should return true given a move with an amphipod in its destination already`() {
+            val a = Amphipod(1, AmphipodType.Amber)
+            val move = SpaceMap.roomSpaces[0].moves.find { it.destination == SpaceMap.hallwaySpaces[0] }!!
+            val amphipodLocations = mapOf(a to move.destination)
+            val position = Position(amphipodLocations)
+
+            assert(position.isBlocked(move))
+        }
+
+        @Test
+        fun `isBlocked should return true given a move with an amphipod in an intermediate step destination already`() {
+            val a = Amphipod(1, AmphipodType.Amber)
+            val move = SpaceMap.roomSpaces[0].moves.find { it.destination == SpaceMap.hallwaySpaces[0] }!!
+            val amphipodLocations = mapOf(a to move.intermediateSpaces.first())
+            val position = Position(amphipodLocations)
+
+
+            assert(position.isBlocked(move))
+        }
+
+        @Test
+        fun `isBlocked should return false given a move with no amphipods in an intermediate step destination already`() {
+            val a = Amphipod(1, AmphipodType.Amber)
+            val move = SpaceMap.roomSpaces[0].moves.find { it.destination == SpaceMap.hallwaySpaces[0] }!!
+            val amphipodLocations = mapOf(a to move.source)
+            val position = Position(amphipodLocations)
+
+
+            assert(!position.isBlocked(move))
+        }
+
+        @Test
+        fun `isFinished should return false given a position that is not complete`() {
+            val position = Position(mapOf())
+
+            assert(!position.isFinished())
+
+        }
+
+        @Test
+        fun `isFinished should return false given a position that is almost complete`() {
+            val a1 = Amphipod(1, AmphipodType.Amber)
+            val a2 = Amphipod(2, AmphipodType.Amber)
+            val b1 = Amphipod(3, AmphipodType.Bronze)
+            val b2 = Amphipod(4, AmphipodType.Bronze)
+            val c1 = Amphipod(5, AmphipodType.Copper)
+            val c2 = Amphipod(6, AmphipodType.Copper)
+            val d1 = Amphipod(7, AmphipodType.Desert)
+            val d2 = Amphipod(8, AmphipodType.Desert)
+
+            val position = Position(mapOf(
+                a1 to SpaceMap.roomSpacesByType[AmphipodType.Amber]!!.first(),
+                a2 to SpaceMap.roomSpacesByType[AmphipodType.Amber]!!.last(),
+                b1 to SpaceMap.roomSpacesByType[AmphipodType.Bronze]!!.first(),
+                b2 to SpaceMap.roomSpacesByType[AmphipodType.Bronze]!!.last(),
+                c1 to SpaceMap.roomSpacesByType[AmphipodType.Copper]!!.first(),
+                c2 to SpaceMap.roomSpacesByType[AmphipodType.Copper]!!.last(),
+                d1 to SpaceMap.roomSpacesByType[AmphipodType.Desert]!!.first(),
+                d2 to SpaceMap.hallwaySpaces[0] //not quite done
+            ))
+
+            assert(!position.isFinished())
+
+        }
+
+        @Test
+        fun `isFinished should return false given a position that is complete`() {
+            val a1 = Amphipod(1, AmphipodType.Amber)
+            val a2 = Amphipod(2, AmphipodType.Amber)
+            val b1 = Amphipod(3, AmphipodType.Bronze)
+            val b2 = Amphipod(4, AmphipodType.Bronze)
+            val c1 = Amphipod(5, AmphipodType.Copper)
+            val c2 = Amphipod(6, AmphipodType.Copper)
+            val d1 = Amphipod(7, AmphipodType.Desert)
+            val d2 = Amphipod(8, AmphipodType.Desert)
+
+            val position = Position(mapOf(
+                a1 to SpaceMap.roomSpacesByType[AmphipodType.Amber]!!.first(),
+                a2 to SpaceMap.roomSpacesByType[AmphipodType.Amber]!!.last(),
+                b1 to SpaceMap.roomSpacesByType[AmphipodType.Bronze]!!.first(),
+                b2 to SpaceMap.roomSpacesByType[AmphipodType.Bronze]!!.last(),
+                c1 to SpaceMap.roomSpacesByType[AmphipodType.Copper]!!.first(),
+                c2 to SpaceMap.roomSpacesByType[AmphipodType.Copper]!!.last(),
+                d1 to SpaceMap.roomSpacesByType[AmphipodType.Desert]!!.first(),
+                d2 to SpaceMap.roomSpacesByType[AmphipodType.Desert]!!.last(),
+            ))
+
+            assert(position.isFinished())
+
+        }
+    }
+
+    @Test
+    fun `parse should return a map of amphipods to initial positions`() {
+        val input = """
+            #############
+            #...........#
+            ###B#C#B#D###
+              #A#D#C#A#
+              #########
+        """.trimIndent().lines()
+
+        val position:Position = parse(input)
+
+        assert(position.reverseMap[SpaceMap.roomSpaces[0]]!!.id == 1)
+        assert(position.reverseMap[SpaceMap.roomSpaces[0]]!!.type == AmphipodType.Bronze)
+        assert(position.reverseMap[SpaceMap.roomSpaces[1]]!!.id == 2)
+        assert(position.reverseMap[SpaceMap.roomSpaces[1]]!!.type == AmphipodType.Amber)
+        assert(position.reverseMap[SpaceMap.roomSpaces[2]]!!.id == 3)
+        assert(position.reverseMap[SpaceMap.roomSpaces[2]]!!.type == AmphipodType.Copper)
+        assert(position.reverseMap[SpaceMap.roomSpaces[3]]!!.id == 4)
+        assert(position.reverseMap[SpaceMap.roomSpaces[3]]!!.type == AmphipodType.Desert)
+        assert(position.reverseMap[SpaceMap.roomSpaces[4]]!!.id == 5)
+        assert(position.reverseMap[SpaceMap.roomSpaces[4]]!!.type == AmphipodType.Bronze)
+        assert(position.reverseMap[SpaceMap.roomSpaces[5]]!!.id == 6)
+        assert(position.reverseMap[SpaceMap.roomSpaces[5]]!!.type == AmphipodType.Copper)
+        assert(position.reverseMap[SpaceMap.roomSpaces[6]]!!.id == 7)
+        assert(position.reverseMap[SpaceMap.roomSpaces[6]]!!.type == AmphipodType.Desert)
+        assert(position.reverseMap[SpaceMap.roomSpaces[7]]!!.id == 8)
+        assert(position.reverseMap[SpaceMap.roomSpaces[7]]!!.type == AmphipodType.Amber)
+    }
+
+    private fun PotentialMove.reverse() : PotentialMove = PotentialMove(destination, intermediateSpaces, source)
 }
-
-
-
-
