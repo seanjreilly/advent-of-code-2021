77d1fa816bbdda8739fa592ce95dc43252a4e6fe
Sean Reilly <seanjreilly@gmail.com>
2022-01-05 11:53:53 +0000

    Implement parse and CucumberGrid

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -1,5 +1,6 @@
 package day25
 
+import utils.gridmap.Point
 import utils.readInput
 
 fun main() {
@@ -15,3 +16,34 @@ fun part1(input: List<String>): Int {
 fun part2(input: List<String>): Int {
     return input.size
 }
+
+internal fun parse(input: List<String>): CucumberGrid {
+    val height = input.size
+    val width = input.first().length
+    val points = mutableMapOf<Point, Cucumber>()
+    input.forEachIndexed { y, line ->
+        require(line.length == width) { "input must be rectangular" }
+        line.forEachIndexed { x, char ->
+            val cucumber = when (char) {
+                '>' -> Cucumber.EAST_FACING
+                'v' -> Cucumber.SOUTH_FACING
+                '.' -> null
+                else -> { throw IllegalArgumentException("unexpected character '${char}'") }
+            }
+            cucumber?.let { points[Point(x,y)] = it }
+        }
+    }
+    return CucumberGrid(points, width, height)
+}
+
+internal class CucumberGrid(private val points: Map<Point, Cucumber>, val width:Int, val height: Int) : Iterable<Map.Entry<Point, Cucumber>> {
+    override fun iterator(): Iterator<Map.Entry<Point, Cucumber>> {
+        return points.iterator()
+    }
+
+    operator fun get(point: Point): Cucumber? {
+        return points[point]
+    }
+}
+
+internal enum class Cucumber { EAST_FACING, SOUTH_FACING }
--- src/test/kotlin/day25/Day25Test.kt
+++ src/test/kotlin/day25/Day25Test.kt
@@ -1,4 +1,22 @@
 package day25
 
+import org.junit.jupiter.api.Test
+import utils.gridmap.Point
+
 class Day25Test {
+    @Test
+    fun `parse should return a sparse grid of south facing and east facing cucumbers given rectangular input`() {
+        val input = """
+            .>.
+            v..
+        """.trimIndent().lines()
+
+        val grid: CucumberGrid = parse(input)
+
+        assert(grid.width == 3)
+        assert(grid.height == 2)
+        assert(grid[Point(1,0)] == Cucumber.EAST_FACING)
+        assert(grid[Point(0,1)] == Cucumber.SOUTH_FACING)
+        assert(grid.count() == 2)
+    }
 }
